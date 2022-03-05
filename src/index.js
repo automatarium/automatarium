f99f0e083c4a293149249887ff72d75d787f0662
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-03-05 18:08:32 +1100

    Add goober for styling and react-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import ReactDOM from 'react-dom'
+import { createElement } from 'react'
+import { setup } from 'goober'
+import { BrowserRouter } from 'react-router-dom'
+
+// Setup goober to use React
+setup(createElement)
 
 ReactDOM.render(
-  <h1>Automatarium</h1>,
+  <BrowserRouter>
+    <h1>Automatarium</h1>
+  </BrowserRouter>,
   document.getElementById('app')
 )
