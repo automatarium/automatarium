e48225b55fdd711cf01862778e8dddd98ba01189
Maxwell Reid <contact@maxwellreid.tech>
2022-03-09 14:31:28 +1100

    Fix all fixable ESlint problems on project

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World ' })
+  return response.json({ message: 'Hello World ' })
 })
 
 app.listen('3001')
