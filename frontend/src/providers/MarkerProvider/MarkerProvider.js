597e9500313294fc6a895d0c87a985ad010a6a28
Ewan Breakey <ebreakey@gmail.com>
2022-03-09 19:53:18 +1100

    Implement parametric arrow heads

--- frontend/src/providers/MarkerProvider/MarkerProvider.js
+++ frontend/src/providers/MarkerProvider/MarkerProvider.js
@@ -1,9 +1,23 @@
 import { useEffect, useRef, createContext } from 'react'
 
+import { ARROWHEAD_ANGLE as a, ARROWHEAD_LENGTH as l } from '/src/config/rendering'
+
 export const MarkerContext = createContext('marker')
 
 const MarkerProvider = ({ children }) => {
+  // Define marker ids
   const standardArrowHead = 'standard-arrow-head'
+
+  // Calculate marker bounding box
+  const markerWidth = 30
+  const markerHeight = 30
+  const markerRefX = markerWidth - 1
+  const markerRefY = markerHeight / 2
+
+  // Calculate arrow verts
+  const arrowStart = `M${markerRefX}, ${markerRefY}`
+  const arrowTopLeft = `L${markerRefX - l * Math.cos(a)} ${markerRefY - l * Math.sin(a)}`
+  const arrowBottomLeft = `L${markerRefX - l * Math.cos(-a)} ${markerRefY - l * Math.sin(-a)}`
 
   return <>
     {/*SVG Definitions*/}
@@ -11,13 +25,13 @@ const MarkerProvider = ({ children }) => {
       {/*Standard Arrowhead Definition*/}
       <marker
         id={standardArrowHead}
-        markerWidth="10"
-        markerHeight="10"
-        refX="8.5"
-        refY="3"
+        markerWidth={markerWidth}
+        markerHeight={markerHeight}
+        refX={markerRefX}
+        refY={markerRefY}
         orient="auto"
         markerUnits="strokeWidth">
-      <path d="M0,0 L0,6 L9,3 z" fill="black" />
+      <path d={`${arrowStart} ${arrowTopLeft} ${arrowBottomLeft} z`} fill="black" />
       </marker>
     </defs>
 
@@ -25,7 +39,6 @@ const MarkerProvider = ({ children }) => {
     <MarkerContext.Provider value={{ standardArrowHead }}>
       { children }
     </MarkerContext.Provider>
-    
   </>
 }
 
