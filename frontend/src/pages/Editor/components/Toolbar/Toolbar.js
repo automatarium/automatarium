4897105946ff7de588d0e948c803dfdadacd77e6
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-03-10 12:32:46 +1100

    More flexible dropdown buttons

--- frontend/src/pages/Editor/components/Toolbar/Toolbar.js
+++ frontend/src/pages/Editor/components/Toolbar/Toolbar.js
@@ -14,14 +14,19 @@ const Toolbar = () => {
     <Wrapper>
       <Menu>
         <Logo />
+
         <div>
+          {/* TODO: Make the title editable */}
           <Name>Example Title</Name>
+
           <DropdownMenus>
-            <Dropdown type="button">File</Dropdown>
-            <Dropdown type="button">Edit</Dropdown>
-            <Dropdown type="button">View</Dropdown>
-            <Dropdown type="button">Tools</Dropdown>
-            <Dropdown type="button">Help</Dropdown>
+            {['File', 'Edit', 'View', 'Tools', 'Help'].map(label => (
+              <Dropdown
+                type="button"
+                key={label}
+                onClick={() => console.log('You clicked', label)}
+              >{label}</Dropdown>
+            ))}
           </DropdownMenus>
         </div>
       </Menu>
