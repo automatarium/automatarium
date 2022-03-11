97720ee5f27c43cfba5c305fc280951fafebd0f8
Thomas Dib <s3838765@student.rmit.edu.au>
2022-03-11 18:48:56 +1100

    Finalise refactor of FSA

--- frontend/src/simulation/FSA.js
+++ frontend/src/simulation/FSA.js
@@ -1,33 +1,23 @@
 const graph = require('./tests/dib-nfa4')
 
-const input = 'dipp'
+const input = 'dippp'
 console.log('INPUT:\t', input)
 
-const simulateFSA = (graph, input, initialStateID) => {
-  // Recursively Simulate
-  const { accepted, trace } = performFSATransition(input, graph, initialStateID)
-
-  return {
-    accepted,
-    trace, // Trace of accepted or longest
-  }
-}
-
-const performFSATransition = (
-  input,
+const simulateFSA = (
   graph,
+  input,
   currStateID = graph?.options?.initialState,
   trace = [graph?.options?.initialState]
 ) => {
   // Are we done processing symbols?
   if (input.length === 0) {
-    const currState = graph.states.find(state => state.id === currStateID)
+    const currState = graph.states.find((state) => state.id === currStateID)
     return { accepted: currState.isFinal, trace }
   }
 
   // Get next set of possible transitions
   const possibleTransitions = graph.transitions.filter(
-    tr => tr.from === currStateID && (tr.read === input[0] || tr.read === '')
+    (tr) => tr.from === currStateID && (tr.read === input[0] || tr.read === '')
   )
 
   // No transitions possible?
@@ -36,18 +26,18 @@ const performFSATransition = (
   }
 
   // Continue recurring
-  const results = possibleTransitions.map(tr =>
-    performFSATransition(
-      input.slice(1), // Slice first character off input
+  const results = possibleTransitions.map((tr) =>
+    simulateFSA(
       graph,
+      input.slice(1), // Slice first character off input
       tr.to, // Transition to next possible state
       [...trace, tr.to] // Append next transition to trace
     )
   )
 
   // Return first accepting trace or longest rejecting
-  const acceptingResult = results.find(r => r.accepted)
-  return acceptingResult || results.sort(result => result.trace.length)[0]
+  const acceptingResult = results.find((r) => r.accepted)
+  return acceptingResult || results.sort((r1, r2) => r2.trace.length - r1.trace.length)[0]
 }
 
 const output = simulateFSA(graph, input)
