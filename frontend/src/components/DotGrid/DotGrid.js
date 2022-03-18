8a905a645f4f049a6ae88c248f9d36270bc321ee
Ewan Breakey <ebreakey@gmail.com>
2022-03-18 18:47:33 +1100

    Improve efficiency of dot grid component

--- frontend/src/components/DotGrid/DotGrid.js
+++ frontend/src/components/DotGrid/DotGrid.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from 'react'
+import { Fragment, useState, useEffect, useMemo } from 'react'
 import { GRID_SNAP } from '/src/config/interactions'
 import { DOT_GRID_RADIUS } from '/src/config/rendering'
 import { useViewStore } from '/src/stores'
@@ -6,40 +6,44 @@ import { useViewStore } from '/src/stores'
 import { DotGridContainer, DotGridCircle } from './dotGridStyle'
 
 const DotGrid = () => {
-  const spacing = GRID_SNAP * 2
+  const spacing = GRID_SNAP * 4
   const [isAltKeyPressed, setIsAltKeyPressed] = useState(false)
   const position = useViewStore(s => s.position)
   const scale = useViewStore(s => s.scale)
   const size = useViewStore(s => s.size)
 
-  const gridSize = [
+  const gridSize = useMemo(() => [
     Math.floor((scale * size.width) / spacing) + 3,
     Math.floor((scale * size.height) / spacing) + 3,
-  ]
+  ], [scale, size])
 
-  const gridOffset = [
+  const gridOffset = useMemo(() => [
     (Math.floor((position.x * scale) / spacing) * spacing) - spacing,
     (Math.floor((position.y * scale) / spacing) * spacing) - spacing,
-  ]
+  ], [position, scale])
 
   useEffect(() => {
     const handleKeyDown = e => setIsAltKeyPressed(e.altKey)
-    const handleKeyUp = e => setIsAltKeyPressed(false)
+    const handleKeyUp = () => setIsAltKeyPressed(false)
     document.addEventListener('keydown', handleKeyDown)
     document.addEventListener('keyup', handleKeyUp)
     return () => {
       document.removeEventListener('keydown', handleKeyDown)
       document.removeEventListener('keyup', handleKeyUp)
     }
-  })
+  }, [])
 
-  const [h, v] = gridSize
-  return <DotGridContainer data-snapping={!isAltKeyPressed}>
-    {Array.from({ length: h }).map((_, i) => <Fragment key={i}>
-      {Array.from({ length: v }).map((_, j) => <Fragment key={j}>
+  // Render and memoize the dots
+  const dots = useMemo(() =>
+    Array.from({ length: gridSize[0] }).map((_, i) => <Fragment key={i}>
+      {Array.from({ length: gridSize[1] }).map((_, j) => <Fragment key={j}>
         <DotGridCircle key={`${i},${j}`} cx={gridOffset[0] + i*spacing} cy={gridOffset[1] + j*spacing} r={DOT_GRID_RADIUS} />
       </Fragment>)}
-    </Fragment>)}
+    </Fragment>)
+  , [gridOffset[0], gridOffset[1], gridSize[0], gridSize[1]])
+
+  return <DotGridContainer data-snapping={!isAltKeyPressed}>
+    {dots}
   </DotGridContainer>
 }
 
