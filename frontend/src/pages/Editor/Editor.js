370adf145f2ca8de745f28eba0030552fc77cbd0
Ewan Breakey <ebreakey@gmail.com>
2022-03-20 17:58:43 +1100

    Setup tool store and use it in the Toolbar

--- frontend/src/pages/Editor/Editor.js
+++ frontend/src/pages/Editor/Editor.js
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-
 import { GraphView } from '/src/components'
-import { Menubar, Sidepanel, Toolbar } from './components'
 
+import { Menubar, Sidepanel, Toolbar } from './components'
 import { Content } from './editorStyle'
 
 const Editor = () => {
-  const [tool, setTool] = useState('Cursor')
-
   return (
     <>
       <Menubar />
 
       <Content>
-        <Toolbar value={tool} onChange={t => setTool(t)} />
+        <Toolbar />
         <GraphView style={{flex: 1}} />
         <Sidepanel />
       </Content>
