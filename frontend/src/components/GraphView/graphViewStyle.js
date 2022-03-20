537ca3f34c9203a16c84fda8d245352b0b16c0af
Ewan Breakey <ebreakey@gmail.com>
2022-03-20 17:21:35 +1100

    Convert DotGrid component to css styling

--- frontend/src/components/GraphView/graphViewStyle.js
+++ frontend/src/components/GraphView/graphViewStyle.js
@@ -2,7 +2,19 @@ import { forwardRef } from 'react'
 import { styled } from 'goober'
 
 export const Svg = styled('svg', forwardRef)`
+
   background: var(--white);
+
+  /* Grid */
+  ${p => p.$showGrid && `
+    --dot-fraction: 12.5%;
+    background: radial-gradient(
+      var(--grid-dot),
+      var(--grid-dot) var(--dot-fraction),
+      var(--white) var(--dot-fraction));
+  `};
+
+  /* Paths */
   stroke-linejoin: round;
   stroke-linecap: round;
   stroke-width: 2px;
