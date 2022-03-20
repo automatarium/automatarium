35122b75de263105e77941d34d1e4f6fc69c9eff
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-03-20 17:02:05 +1100

    Add sidepanel framework

---
+++ frontend/src/pages/Editor/components/Sidepanel/sidepanelStyle.js
@@ -0,0 +1,11 @@
+import { styled } from 'goober'
+
+export const Wrapper = styled('div')`
+  display: flex;
+`
+
+export const Panel = styled('div')`
+  min-width: 300px;
+  overflow-y: auto;
+  background: var(--surface);
+`
--- frontend/src/pages/Editor/components/index.js
+++ frontend/src/pages/Editor/components/index.js
@@ -1,3 +1,4 @@
 export { default as Menubar } from './Menubar/Menubar'
 export { default as Sidebar } from './Sidebar/Sidebar'
 export { default as Toolbar } from './Toolbar/Toolbar'
+export { default as Sidepanel } from './Sidepanel/Sidepanel'
