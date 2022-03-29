90cf4c5457b60f91c7479443911371cd7376282b
Maxwell Reid <contact@maxwellreid.tech>
2022-03-29 17:25:14 +1100

    Fixed async await layout for project routes

--- backend/src/controllers/project.ts
+++ backend/src/controllers/project.ts
@@ -1,31 +1,30 @@
 import { NextFunction, Request, Response } from 'express'
 
-import Project from '../models/project'
+import Project from 'models/project'
 
-export const getProject = ( req: Request, res: Response, next: NextFunction ) => {
+export const getProject = async ( req: Request, res: Response, next: NextFunction ) => {
   const { pid } = req.params
 
   // Retrieve project by id
-  Project.findById(pid)
-    .exec()
-    .then((result) => {
-      return res.status(200).json({
-        automaton: result
-      })
-    }).catch((error) => {
-      return res.status(500).json({
-        message: error.message,
-        error
-      })
+  try {
+    const project = await Project.findById(pid).orFail()
+    return res.status(200).json({
+      automaton: project
     })
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+      error
+    })
+  }
 }
 
-export const createProject = ( req: Request, res: Response, next: NextFunction ) => {  
+export const createProject = async ( req: Request, res: Response, next: NextFunction ) => {  
   const { id, userid, isPublic, meta, initialState, states, transitions, comments, tests } = req.body
   
   // Create new project
   const project = new Project({
-    id,
+    _id: id,
     userid,
     isPublic,
     meta,
@@ -37,15 +36,16 @@ export const createProject = ( req: Request, res: Response, next: NextFunction )
   })
 
   // Save project
-  return project.save()
-    .then( (result) => {
-      return res.status(201).json({
-        automaton: result
-      })
-    }).catch((error) => {
-      return res.status(500).json({
-        message: error.message,
-        error
-      })
+  try {
+    await project.save()
+    return res.status(201).json({
+      project
     })
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+      error
+    })
+  }
+  // Save project
 }
--- backend/src/interfaces/project.ts
+++ backend/src/interfaces/project.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose'
 import { comment, tests, meta } from 'types/main'
 
 export default interface IProject extends Document {
-  id: string
+  _id: string
   userid: string
   isPublic: boolean
   meta: meta
