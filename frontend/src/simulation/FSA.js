e13712cf54e72fde80b8543cba2f9103d83382da
Thomas Dib <s3838765@student.rmit.edu.au>
2022-04-01 22:24:47 +1100

    Implement lambda transition handling

--- frontend/src/simulation/FSA.js
+++ frontend/src/simulation/FSA.js
@@ -1,24 +1,29 @@
-const graph = require('./tests/dib-nfa4')
+const graph = require('./tests/dib-nfa4_lambdaloop')
 
-const input = 'dippp'
+const input = 'dip'
 console.log('INPUT:\t', input)
 
 const simulateFSA = (
   graph,
   input,
   currStateID = graph?.options?.initialState,
-  trace = [graph?.options?.initialState]
+  trace = [graph?.options?.initialState],
+  lambdaCount = 0,
+  lastTransitionLambda = false
 ) => {
-  // Are we done processing symbols?
+  // Are we done processing symbols? - what if there is a lambda from current state
   if (input.length === 0) {
-    const currState = graph.states.find((state) => state.id === currStateID)
+    const currState = graph.states.find(state => state.id === currStateID)
     return { accepted: currState.isFinal, trace }
   }
 
   // Get next set of possible transitions
   const possibleTransitions = graph.transitions.filter(
-    (tr) => tr.from === currStateID && (tr.read === input[0] || tr.read === '')
+    tr => tr.from === currStateID && (tr.read === input[0] || (tr.read === '' && lambdaCount < 100))
   )
+
+  // Move lambda transitions to end of array (to prioritise non-lambda transitions)
+  possibleTransitions.sort((a, b) => b.read.length - a.read.length)
 
   // No transitions possible?
   if (possibleTransitions.length === 0) {
@@ -26,17 +31,19 @@ const simulateFSA = (
   }
 
   // Continue recurring
-  const results = possibleTransitions.map((tr) =>
+  const results = possibleTransitions.map(tr =>
     simulateFSA(
       graph,
-      input.slice(1), // Slice first character off input
+      tr.read === '' ? input : input.slice(1), // Slice first character off input
       tr.to, // Transition to next possible state
-      [...trace, tr.to] // Append next transition to trace
+      [...trace, tr.to], // Append next transition to trace
+      tr.read === '' && lastTransitionLambda ? lambdaCount + 1 : 0, // Increment lambda counter
+      tr.read === '' // Tell the next recurse whether this transition was a lambda
     )
   )
 
   // Return first accepting trace or longest rejecting
-  const acceptingResult = results.find((r) => r.accepted)
+  const acceptingResult = results.find(r => r.accepted)
   return acceptingResult || results.sort((r1, r2) => r2.trace.length - r1.trace.length)[0]
 }
 
