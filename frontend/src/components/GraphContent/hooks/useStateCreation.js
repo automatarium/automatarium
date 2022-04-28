e1bbda095eb34ec75915f06f209c47487f1c2f9d
Ewan Breakey <ebreakey@gmail.com>
2022-04-28 16:45:13 +1000

    Commit state and transition creating to history

--- frontend/src/components/GraphContent/hooks/useStateCreation.js
+++ frontend/src/components/GraphContent/hooks/useStateCreation.js
@@ -6,12 +6,14 @@ const useStateCreation = ({ containerRef }) => {
   const tool = useToolStore(s => s.tool)
   const toolActive = tool === 'state'
   const createState = useProjectStore(s => s.createState)
+  const commit = useProjectStore(s => s.commit)
   const screenToViewSpace = useViewStore(s => s.screenToViewSpace)
 
   const handleMouseDown = useCallback(e => {
     if (e.target === containerRef.current && toolActive) {
       const [vx, vy] = screenToViewSpace(e.clientX, e.clientY, containerRef.current)
       createState({ x: vx, y: vy })
+      commit()
     }
   }, [toolActive, containerRef?.current])
 
