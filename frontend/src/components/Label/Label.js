fecb46501c4aeede3564c5a7437ddbba5e2b6062
Ewan Breakey <ebreakey@gmail.com>
2022-05-23 21:24:22 +1000

    Remove unused css

--- frontend/src/components/Label/Label.js
+++ frontend/src/components/Label/Label.js
@@ -3,9 +3,6 @@ import { styled } from 'goober'
 const Label = styled('label')`
   display: block;
   margin-bottom: .3em;
-  /* &::after { */
-    /* content: '${props => !props.$required ? ' (optional)' : ''}'; */
-  /* } */
 `
 
 export default Label
