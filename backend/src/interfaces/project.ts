8946ef4c78cdc9734175e03b917b4d4579eec738
Maxwell Reid <contact@maxwellreid.tech>
2022-05-23 14:42:28 +1000

    Rename types to pascal case

--- backend/src/interfaces/project.ts
+++ backend/src/interfaces/project.ts
@@ -1,16 +1,16 @@
 import { Document } from 'mongoose'
 
-import { comment, tests, meta } from 'types'
+import { Comment, Tests, Meta } from 'types'
 
 export default interface IProject extends Document {
   _id: string
   userid: string
   isPublic: boolean
-  meta: meta
+  meta: Meta
   config: object
   initialState: number
   states: object[]
   transitions: object[]
-  comments: comment[]
-  tests: tests
+  comments: Comment[]
+  tests: Tests
 }
