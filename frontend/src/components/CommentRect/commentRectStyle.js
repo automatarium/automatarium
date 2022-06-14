25d9372643dac35f3e785ee60b2ca3d5d70c0a7f
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-06-14 20:30:12 +1000

    Use white for comment text in dark mode

--- frontend/src/components/CommentRect/commentRectStyle.js
+++ frontend/src/components/CommentRect/commentRectStyle.js
@@ -1,7 +1,7 @@
 export const commentStyles = {
-  color: 'var(--stroke)',
+  color: 'var(--comment-text)',
   background: 'var(--grid-bg)',
-  padding: '1em',
+  padding: '.8em 1em',
   borderRadius: '.5rem',
   borderWidth: '2.5px',
   borderStyle: 'solid',
