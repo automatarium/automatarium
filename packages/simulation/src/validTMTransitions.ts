aaf792b26255e3dba18c8ca43db5d0b9c7317e92
ConorChristensen-RMIT <s3815282@student.rmit.edu.au>
2022-09-15 11:16:11 +1000

    Deprecating this branch after Tomas' simulation improvements. Easier to branch from his and manually transfer progress.

--- packages/simulation/src/validTMTransitions.ts
+++ packages/simulation/src/validTMTransitions.ts
@@ -1,5 +1,5 @@
 import {FSAGraph, StateID, ReadSymbol, Transition, TMTransition, TMGraph} from './types.d'
-import closureWithPredicate from './closureWithPredicate'
+import closureWithPredicateTM from './closureWithPredicateTM'
 
 export type ValidTMTransition = { transition: TMTransition, trace: { to: number, read: string }[]}
 
@@ -9,48 +9,49 @@ export type ValidTMTransition = { transition: TMTransition, trace: { to: number,
  *
  * TO DO. IN DEVELOPMENT TO MOVE FROM FSA TO TM
  *
- * @param graph - The FSA graph object used as input
+ * @param graph - The TM graph object used as input
  * @param currentStateID - The ID of the current state used to compute the valid transitions
  * @param nextRead  - The input symbol to be read next
  * @returns A list of transitions and the "trace" of states and symbols required to navigate it.
  */
 export const validTMTransitions = (graph: TMGraph, currentStateID: StateID, nextRead: ReadSymbol): ValidTMTransition[] => {
   // Compute lambda closure (states accessible without consuming input)
-  const closure = Array.from(closureWithPredicate(graph, currentStateID, tr => tr.read.length === 0))
+  // const closure = Array.from(closureWithPredicateTM(graph, currentStateID, tr => tr.readSymbol.length === 0))
 
   // Find direct non-lambda transitions
   const directTransitions = graph.transitions
     .filter(transition => transition.from === currentStateID &&
-                         transition.read.some(symbol => symbol === nextRead))
+                         transition.readSymbol.some(symbol => symbol === nextRead))
     .map(transition => ({ transition, trace: [] }))
 
   // Find transitions from states in lambda closure that we can take
-  const indirectTransitions = closure
-    .map(([stateID, precedingTransitions]) => graph.transitions
-      .filter(transition => transition.from === stateID &&
-                           transition.read.some(symbol => symbol === nextRead))
-      .map(transition => ({ transition, trace: precedingTransitions })))
-    .reduce((a, b) => [...a, ...b], [])
+  // const indirectTransitions = closure
+  //   .map(([stateID, precedingTransitions]) => graph.transitions
+  //     .filter(transition => transition.from === stateID &&
+  //                          transition.readSymbol.some(symbol => symbol === nextRead))
+  //     .map(transition => ({ transition, trace: precedingTransitions })))
+  //   .reduce((a, b) => [...a, ...b], [])
 
   // Find transitions to final states in lambda closure
-  const finalTransitions = closure
-    .filter(([stateID]) => graph.states.some(s => s.id === stateID && s.id !== currentStateID && s.isFinal))
-    .map(([stateID, precedingTransitions]) => ({
-      transition: (graph.transitions.find(tr => tr.to === stateID && tr.read.length === 0) as Transition),
-      trace: precedingTransitions.slice(0, -1),
-    }))
+  // const finalTransitions = closure
+  //   .filter(([stateID]) => graph.states.some(s => s.id === stateID && s.id !== currentStateID && s.isFinal))
+  //   .map(([stateID, precedingTransitions]) => ({
+  //     transition: (graph.transitions.find(tr => tr.to === stateID && tr.readSymbol.length === 0) as TMTransition),
+  //     trace: precedingTransitions.slice(0, -1),
+  //   }))
 
   // Combine transitions
   const allTransitions = [
-    ...directTransitions,
-    ...indirectTransitions,
-    ...finalTransitions,
+    ...directTransitions//,
+    // ...indirectTransitions,
+    // ...finalTransitions,
   ]
 
   // Format trace, add final transition to trace and return
   return allTransitions
-    .map(({ transition, trace }) => ({
+    .map(({transition, trace }) => ({
       transition,
-      trace: [...trace, transition].map(tr => ({ to: tr.to, read: tr.read.length === 0 ? '' : nextRead }))
+        //I think below is where I need to add directionality. Not just jump to next read.
+      trace: [...trace, transition].map(tr => ({ to: tr.to, read: tr.readSymbol.length === 0 ? '' : nextRead }))
     }))
 }
