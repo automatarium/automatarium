dbbaffd416ecb94feb1b2bb6bc536c74d9a6062e
Jake Leahy <jake@leahy.dev>
2023-03-08 00:56:39 +1100

    Add linting to simulator-v2

--- packages/simulation-v2/src/PDASearch.ts
+++ packages/simulation-v2/src/PDASearch.ts
@@ -1,121 +1,112 @@
-import { PDATransition, Stack } from "./graph";
-import { Graph, Node, State } from "./interfaces/graph";
+import { PDATransition, Stack } from './graph'
+import { Graph, Node, State } from './interfaces/graph'
 
 export class PDAState extends State {
-    constructor(
-        m_id: number,
-        m_isFinal: boolean,
-        private m_read: string | null = null,
-        private m_remaining: string = "",
-        private m_stack: Stack = [],
-        private m_pop: string = "",
-        private m_push: string = "",
-    ) {
-        super(m_id, m_isFinal);
-    }
+  constructor (
+    id: number,
+    isFinal: boolean,
+        private read: string | null = null,
+        private remaining: string = '',
+        private stack: Stack = [],
+        private pop: string = '',
+        private push: string = ''
+  ) {
+    super(id, isFinal)
+  }
 
-    get read() {
-        return this.m_read;
-    }
+  get read () {
+    return this.read
+  }
 
-    get remaining() {
-        return this.m_remaining;
-    }
+  get remaining () {
+    return this.remaining
+  }
 
-    get stack() {
-        return this.m_stack;
-    }
-    get pop() {
-        return this.m_pop;
-    }
+  get stack () {
+    return this.stack
+  }
 
-    get push() {
-        return this.m_push;
-    }
+  get pop () {
+    return this.pop
+  }
 
-    key() {
-        return String(this.id + this.remaining + this.stack.join(""));
-    }
+  get push () {
+    return this.push
+  }
+
+  key () {
+    return String(this.id + this.remaining + this.stack.join(''))
+  }
 }
 
 export class PDAGraph extends Graph<PDAState, PDATransition> {
-    constructor(
-        initial: Node<PDAState>,
-        states: PDAState[],
-        transitions: PDATransition[],
-    ) {
-        super(initial, states, transitions);
-    }
-    public isFinalState(node: Node<PDAState>) {
-        return node.state.isFinal &&
+  public isFinalState (node: Node<PDAState>) {
+    return node.state.isFinal &&
                node.state.remaining.length === 0 &&
-               node.state.stack.length === 0;
-    }
+               node.state.stack.length === 0
+  }
 
-    public getSuccessors(node: Node<PDAState>) {
-        const transitions = this.transitions.filter(
-            (transition) => transition.from === node.state.id,
-        );
-        const successors: Node<PDAState>[] = [];
-        
-        for (const transition of transitions) {
-            
-            const nextState = this.states.find(
-                (state) => state.id === transition.to,
-            );
-            let invalidPop = false;
-            const lambdaTransition = transition.read.length === 0;
-            const symbol = node.state.remaining[0];
-            const pop = transition.pop;
-            const push = transition.push;
+  public getSuccessors (node: Node<PDAState>) {
+    const transitions = this.transitions.filter(
+      (transition) => transition.from === node.state.id
+    )
+    const successors: Node<PDAState>[] = []
 
-            // If there is no next state
-            if (
-                nextState === undefined ||
+    for (const transition of transitions) {
+      const nextState = this.states.find(
+        (state) => state.id === transition.to
+      )
+      let invalidPop = false
+      const lambdaTransition = transition.read.length === 0
+      const symbol = node.state.remaining[0]
+      const pop = transition.pop
+      const push = transition.push
+
+      // If there is no next state
+      if (
+        nextState === undefined ||
                 (!lambdaTransition && !transition.read.includes(symbol))
-            ) {
-                continue;
-            }
+      ) {
+        continue
+      }
 
-            // Check valid stack operations
-            let nodeStack = node.state.stack.slice(); // the stack carries forward to each node
-            // Handle pop symbol first
-            if (pop !== '') {
-                // Pop if symbol matches top of stack
-                if(pop === nodeStack[nodeStack.length - 1]) {
-                    nodeStack.pop();
-                }
-                // Else operation is invalid
-                // Empty stack case
-                else if (nodeStack.length === 0) {
-                    invalidPop = true;
-                }
-                // Non-matching symbol case
-                else if (pop !== nodeStack[nodeStack.length - 1]) {
-                    invalidPop = true;
-                }  
-            }
-            // Handle push symbol if it exists
-            if (push !== '') {
-                nodeStack.push(push);
-            }
-            // If stack operations were valid, add the successor
-            if (!invalidPop) {
-                const graphState = new PDAState(
-                    nextState.id,
-                    nextState.isFinal,
-                    lambdaTransition ? "" : symbol,
-                    lambdaTransition
-                        ? node.state.remaining
-                        : node.state.remaining.slice(1),
-                    nodeStack, // the stack carries forward to each node
-                    pop,
-                    push,
-                );
-                const successor = new Node(graphState, node);
-                successors.push(successor);
-            }
+      // Check valid stack operations
+      const nodeStack = node.state.stack.slice() // the stack carries forward to each node
+      // Handle pop symbol first
+      if (pop !== '') {
+        // Pop if symbol matches top of stack
+        if (pop === nodeStack[nodeStack.length - 1]) {
+          nodeStack.pop()
+        } else if (nodeStack.length === 0) {
+          // Else operation is invalid
+          // Empty stack case
+          invalidPop = true
+        } else if (pop !== nodeStack[nodeStack.length - 1]) {
+          // Non-matching symbol case
+          invalidPop = true
         }
-        return successors;
+      }
+      // Handle push symbol if it exists
+      if (push !== '') {
+        nodeStack.push(push)
+      }
+      // If stack operations were valid, add the successor
+      if (!invalidPop) {
+        const graphState = new PDAState(
+          nextState.id,
+          nextState.isFinal,
+          lambdaTransition ? '' : symbol,
+          lambdaTransition
+            ? node.state.remaining
+            : node.state.remaining.slice(1),
+          nodeStack, // the stack carries forward to each node
+          pop,
+          push
+        )
+        const successor = new Node(graphState, node)
+        successors.push(successor)
+      }
     }
+    return successors
+  }
 }
--- packages/simulation-v2/src/interfaces/graph.ts
+++ packages/simulation-v2/src/interfaces/graph.ts
@@ -1,52 +1,45 @@
-import { Transition } from "../graph";
+import { Transition } from '../graph'
 
 export abstract class State {
-    constructor(private m_id: number, private m_isFinal: boolean) {}
+  get id () {
+    return this.id
+  }
 
-    get id() {
-        return this.m_id;
-    }
+  get isFinal () {
+    return this.isFinal
+  }
 
-    get isFinal() {
-        return this.m_isFinal;
-    }
-
-    abstract key(): string;
+  abstract key(): string;
 }
 
 export class Node<S extends State> {
-    private m_depth: number;
-    constructor(
-        protected m_state: S,
-        protected m_parent: Node<S> | null = null,
-    ) {
-        this.m_depth = m_parent ? m_parent.depth + 1 : 0;
-    }
+  private _depth: number
+  constructor (
+        protected state: S,
+        protected parent: Node<S> | null = null
+  ) {
+    this._depth = parent ? parent.depth + 1 : 0
+  }
 
-    get depth() {
-        return this.m_depth;
-    }
+  get depth () {
+    return this._depth
+  }
 
-    get state() {
-        return this.m_state;
-    }
+  get state () {
+    return this.state
+  }
 
-    get parent() {
-        return this.m_parent;
-    }
+  get parent () {
+    return this.parent
+  }
 }
 
+// eslint-disable-next-line no-unused-vars
 export abstract class Graph<S extends State, T extends Transition> {
-    constructor(
-        protected m_initial: Node<S>,
-        protected states: S[],
-        protected transitions: T[],
-    ) {}
+  get initial () {
+    return this.initial
+  }
 
-    get initial() {
-        return this.m_initial;
-    }
-
-    abstract getSuccessors(node: Node<S>): Node<S>[];
-    abstract isFinalState(node: Node<S>): boolean;
+  abstract getSuccessors(node: Node<S>): Node<S>[];
+  abstract isFinalState(node: Node<S>): boolean;
 }
