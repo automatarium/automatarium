8042e40c11a8b6df9fa7dd20394813775b3ca696
“SidhraFernando-Plant <s3873756@student.rmit.edu.au>
2023-03-23 11:33:05 +1100

    Make new proj thumbnails change background with theme

--- frontend/src/pages/NewFile/components/NewProjectCard/NewProjectCard.js
+++ frontend/src/pages/NewFile/components/NewProjectCard/NewProjectCard.js
@@ -1,9 +1,11 @@
 import { CardContainer, CardImage, CardContent } from './newProjectCardStyle'
+import { usePreferencesStore } from '/src/stores'
 
 const NewProjectCard = ({ title, description, image, ...props }) => {
+  const preferences = usePreferencesStore(state => state.preferences)
   return (
     <CardContainer {...props}>
-      <CardImage $disabled={props.disabled}>{image}</CardImage>
+      <CardImage $disabled={props.disabled} theme={preferences.theme}>{image}</CardImage>
       <CardContent>
         <strong>{title}</strong>
         <p>{description}</p>
