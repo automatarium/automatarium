545f534f3956dbd2d84372173ee283e99f78ca78
Jake Leahy <jake@leahy.dev>
2023-03-27 19:01:00 +1100

    Add tests for cycle and that sorting is done

--- packages/simulation/tests/reorderGraph.test.ts
+++ packages/simulation/tests/reorderGraph.test.ts
@@ -1,6 +1,7 @@
 import { describe } from 'node:test'
 import { UnparsedGraph } from '../src/graph'
 import { reorderStates } from '../src/reorder'
+import dibDipLambdaLoop from './graphs/dib_dip-lambdaloop.json'
 
 describe('Reordering graph', () => {
   test('Simple graph can be rearranged', () => {
@@ -48,5 +49,52 @@ describe('Reordering graph', () => {
       direction: 'R',
       read: ''
     })
+  })
+
+  test('Cycles are handled', () => {
+    const testVer = JSON.parse(JSON.stringify(dibDipLambdaLoop)) as UnparsedGraph
+    reorderStates(testVer)
+    expect(testVer).toMatchObject(dibDipLambdaLoop)
+  })
+
+  test('Lower ID path is taken first', () => {
+    const testVer = JSON.parse(JSON.stringify(dibDipLambdaLoop)) as UnparsedGraph
+    // We have to update both states and transitions. We will apply this mapping
+    const mapping = {
+      0: 2,
+      1: 3,
+      2: 6,
+      3: 7,
+      4: 4,
+      5: 100,
+      6: 200
+    }
+    testVer.initialState = mapping[0]
+    for (const old in mapping) {
+      const newVal = mapping[old]
+      const oldVal = parseInt(old)
+      testVer.states[oldVal].id = newVal
+      dibDipLambdaLoop.transitions.forEach((x, i) => {
+        const trans = testVer.transitions[i]
+        if (x.from === oldVal) trans.from = newVal
+        if (x.to === oldVal) trans.to = newVal
+      })
+    }
+
+    testVer.states[1].id = 3
+    // Make the lower path have lower value
+    testVer.states[4].id = 4
+    testVer.states[2].id = 6
+    testVer.states[3].id = 7
+    testVer.states[5].id = 100
+    testVer.states[6].id = 200
+    reorderStates(testVer)
+
+    expect(testVer.states[1].id).toBe(1)
+    expect(testVer.states[4].id).toBe(2)
+    expect(testVer.states[5].id).toBe(3)
+    expect(testVer.states[6].id).toBe(4)
+    expect(testVer.states[2].id).toBe(5)
+    expect(testVer.states[3].id).toBe(6)
   })
 })
