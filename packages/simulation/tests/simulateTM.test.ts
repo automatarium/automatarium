b6bc710b971e845290a9999a4a01f723c425a09f
Jake Leahy <jake@leahy.dev>
2023-03-27 11:28:55 +1100

    Add another test case. This machine shouldn't crash the program

--- packages/simulation/tests/simulateTM.test.ts
+++ packages/simulation/tests/simulateTM.test.ts
@@ -1,5 +1,6 @@
 import a2b2a from './graphs/a2b2a.json'
 import shuffleLeft from './graphs/shuffleLeft.json'
+import bepsi from './graphs/bepsi.json'
 import { simulateTM } from '../src/simulateTM'
 import { TMExecutionResult, TMGraphIn } from '../src/graph'
 import { describe } from 'node:test'
@@ -22,10 +23,28 @@ describe('Machine that moves left', () => {
   })
 })
 
-describe('Turing machine that converts A to B then resets tape', () => {
+describe('Machine that converts A to B then resets tape', () => {
   test('Accepts AA', () => {
     const result = simulate(a2b2a, 'AA')
     expect(result.tape).toMatchObject({ pointer: 0, trace: ['A', 'A', ''] })
+    expect(result.halted).toBeTrue()
+  })
+})
+
+describe('Machine that must have Bs either side and can have Cs in the middle', () => {
+  test('Accepts BB', () => {
+    const result = simulate(bepsi, 'BB')
+    expect(result.halted).toBeTrue()
+  })
+
+  test('Accepts BCB', () => {
+    const result = simulate(bepsi, 'BB')
+    expect(result.halted).toBeTrue()
+  })
+
+  test('Tracing is correct for BCB', () => {
+    const result = simulate(bepsi, 'BCB')
+    expect(result.tape).toMatchObject({ pointer: 3, trace: ['B', 'C', 'A'] })
     expect(result.halted).toBeTrue()
   })
 })
