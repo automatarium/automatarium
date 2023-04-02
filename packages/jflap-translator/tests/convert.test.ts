5974912f67f5fe37f5ff871ba6cd94d26db22ce1
Jake Leahy <jake@leahy.dev>
2023-04-02 18:39:47 +1000

    Add PDA related test

--- packages/jflap-translator/tests/convert.test.ts
+++ packages/jflap-translator/tests/convert.test.ts
@@ -58,3 +58,84 @@ describe('Importing single attribute', () => {
     ])
   })
 })
+
+describe('Import single attribute multiple states', () => {
+  const machine = readProject('single-attribute-multiple-states')
+
+  test('States are imported', () => {
+    expect(machine.states).toMatchObject([
+      {
+        id: 0,
+        label: '',
+        name: 'q0',
+        x: 162,
+        y: 139,
+        isFinal: true
+      },
+      {
+        id: 1,
+        label: '',
+        name: 'q1',
+        x: 238,
+        y: 261,
+        isFinal: false
+      }
+    ])
+    expect(machine.initialState).toBe(0)
+  })
+
+  test('Transitions are imported', () => {
+    expect(machine.transitions).toMatchObject([
+      {
+        id: 0,
+        from: 0,
+        to: 0,
+        read: ''
+      },
+      {
+        id: 1,
+        from: 0,
+        to: 0,
+        read: 'a'
+      },
+      {
+        id: 2,
+        from: 0,
+        to: 1,
+        read: 'd'
+      }
+    ])
+  })
+})
+
+describe('Import a PDA', function () {
+  const machine = readProject('simple-pda')
+
+  test('Config is correct', () => {
+    expect(machine.config).toMatchObject({
+      type: 'PDA',
+      statePrefix: 'q'
+    })
+  })
+
+  test('Transitions are imported', () => {
+    expect(machine.transitions).toMatchObject([
+      {
+        id: 0,
+        from: 0,
+        to: 0,
+        read: 'A',
+        push: 'A',
+        pop: ''
+      },
+      {
+        id: 1,
+        from: 0,
+        to: 0,
+        read: 'B',
+        push: '',
+        pop: 'A'
+      }
+    ])
+  })
+})
