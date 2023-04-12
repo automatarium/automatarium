3300d2c56c236382e5fab1b09b5711d542461be0
LvandoApps <lachlanvdk55@gmail.com>
2023-04-12 21:54:01 +1000

    Create a mapping for common symbol-to states

--- packages/simulation/src/convert.ts
+++ packages/simulation/src/convert.ts
@@ -37,30 +37,59 @@ export const initialStateIsPresent = (nfaGraphInitialState: StateID): boolean =>
     }
 }
 
+// This will create a mapping of the symbols and the common "to" states, this will enable new states to be created if needed for the DFA
+export function createSymbolsToStateMap(initialTransitionTable: {[key: StateID]: [StateID, ReadSymbol][]}): {[key: ReadSymbol]: StateID[]} {
+    let symbolToStatesMap: {[key: ReadSymbol]: StateID[]} = {};
+    for (let fromStateID in initialTransitionTable) {
+        let transitions = initialTransitionTable[fromStateID];
+        for (let i = 0; i < transitions.length; i++) {
+            let toStateID = transitions[i][0];
+            let symbol = transitions[i][1];    
+            if (symbolToStatesMap.hasOwnProperty(symbol)) {
+                symbolToStatesMap[symbol].push(toStateID);
+            } 
+            else {
+                symbolToStatesMap[symbol] = [toStateID];
+            }
+        }
+    }
+    return symbolToStatesMap;
+}
+
 // This will create a transition table such that the DFA can be constructed from it. It will return a transitionTable that consists of keys of arrays of key value pairs, where
 // the number of keys is equal to the number of states (each key equal to a StateID), where each key will then consist of an array of key value pairs, where the key in this case
 // is a StateID of the state the original key (or state in this case) transitions to, and the value is the ReadSymbol for this transition.
 export function createTransitionTable(nfaGraph: FSAGraphIn, numberOfNFATransitions: number, numberOfNFAStates: number): {[key: StateID]: [StateID, ReadSymbol][]} {
-    let transitionTable: {[key: StateID]: [StateID, ReadSymbol][]} = {}
+    let initialTransitionTable: {[key: StateID]: [StateID, ReadSymbol][]} = {};
+    let finalTransitionTable: {[key: StateID]: [StateID, ReadSymbol][]} = {};
+    let symbolToStatesMap: {[key: ReadSymbol]: StateID[]} = {};
+
+    // This will create the initial transition table. Note that this is not the final transition table as this is still in NFA form.
     for (let curElem = 0; curElem < numberOfNFAStates; curElem++) {
-        transitionTable[curElem] = [];
+        initialTransitionTable[curElem] = [];
+        finalTransitionTable[curElem] = [];
         for (let curStateID = 0; curStateID < numberOfNFATransitions; curStateID++) {
             if (nfaGraph.transitions[curStateID].from == curElem) {
-                transitionTable[curElem].push([nfaGraph.transitions[curStateID].to, nfaGraph.transitions[curStateID].read[0]])
+                initialTransitionTable[curElem].push([nfaGraph.transitions[curStateID].to, nfaGraph.transitions[curStateID].read[0]]);
             }
         }
     }
-    return transitionTable;
+    
+    symbolToStatesMap = createSymbolsToStateMap(initialTransitionTable);
+    console.log("Common Symbol States");
+    console.log(symbolToStatesMap);
+    // This will create the finalTransitionTable from the initial one, which will represent the transition table of the new DFA.
+    console.log("Initial Transition Table");
+    console.log(initialTransitionTable);
+    return initialTransitionTable;
 }
 
 // This will create the DFA and return it by updating a passed in DFA template from a passed in NFA
 export const createDFA = (nfaGraph: FSAGraphIn, dfaGraph: FSAGraphIn): FSAGraphIn => {
   const numberOfNFATransitions: number = nfaGraph.transitions.length;
   const numberOfNFAStates: number = nfaGraph.states.length;
-  let numberOfDFATransitions: number;
 
   let transitionTable: {[key: StateID]: [StateID, ReadSymbol][]} = createTransitionTable(nfaGraph, numberOfNFATransitions, numberOfNFAStates);
-  console.log(transitionTable);
   return dfaGraph;
 }
 
