330a629b0fec68cc38e9532544a400dbf0dd0848
Jake Leahy <jake@leahy.dev>
2023-04-19 14:56:36 +1000

    Switch the transition types into a variant to better handle the different possible types.
    This also makes the types more sound

--- frontend/src/types/ProjectTypes.ts
+++ frontend/src/types/ProjectTypes.ts
@@ -51,29 +51,83 @@ export interface AutomataTests {
  */
 export type TMDirection = 'L' | 'R' | 'S'
 
-// May be different types for each automata
+/**
+ * Basic transition type shared across every transition
+ */
 export interface AutomataTransition {
     from: number,
     id: number,
-    read: string,
+    read: string | string[], // Expanded FSA has multiple read symbols. The access is still the same so shouldn't be a problem
     to: number,
-    direction?: TMDirection,
-    write?: string,
-    push?: string,
-    pop?: string,
+
 }
 
-export interface Project {
+/**
+ * Transition used by PDA projects
+ */
+export interface PDAAutomataTransition extends AutomataTransition{
+    push: string
+    pop: string
+}
+
+/**
+ * Transition used by TM projects
+ */
+export interface TMAutomataTransition extends AutomataTransition {
+    write: string
+}
+
+/**
+ * What the graph used by the frontend looks like.
+ * This is the base type, with variants for the other project types defined later
+ */
+type BaseProjectGraph = {
+    projectType: ProjectType
+    states: AutomataState[]
+    transitions: AutomataTransition[]
+    initialState: number | null
+}
+
+/**
+ * Graph for FSA project. There isn't any different in types but this is just for completion’s sake
+ */
+type FSAProjectGraph = BaseProjectGraph & {
+    projectType: 'FSA'
+}
+
+/**
+ * Graph for PDA project. The transitions need push/pop properties
+ */
+type PDAProjectGraph = BaseProjectGraph & {
+    projectType: 'PDA'
+    transitions: PDAAutomataTransition
+}
+
+/**
+ * Graph for TM project. The transitions need read/write properties
+ */
+type TMProjectGraph = BaseProjectGraph & {
+    projectType: 'TM'
+    transitions: TMAutomataTransition
+}
+
+/**
+ * All the different types a project can be.
+ * This allows for the transitions types to be different
+ */
+type ProjectGraph = FSAProjectGraph | PDAProjectGraph | TMProjectGraph
+
+/**
+ * What a project for the frontend looks like.
+ * This contains everything that is needed to display a graph
+ */
+export type Project = ProjectGraph & {
     comments: ProjectComment[],
     config: ProjectConfig,
-    initialState: number | null,
     meta: ProjectMetaData,
     projectType: string,
-    // Not sure about type of array
     simResult: string[],
-    states: AutomataState[],
     tests: AutomataTests,
-    transitions: AutomataTransition[],
 }
 
 // This is for copy/paste function which isn't TS converted yet
