09709ce81765013c2e64e5d8f262538ce99b14b5
Jake Leahy <jake@leahy.dev>
2023-04-25 21:21:51 +1000

    Only check the fields that are returned by the converter
    Also remove the `as FSAProjectGraph` because I was too lazy to write that everywhere

--- packages/simulation/tests/convertNFAtoDFA.test.ts
+++ packages/simulation/tests/convertNFAtoDFA.test.ts
@@ -16,55 +16,61 @@ import convertSingleTrapState from './graphs/convertSingleTrapState.json'
 import convertSingleTrapStateDFA from './graphs/convertSingleTrapStateDFA.json'
 import { FSAProjectGraph } from 'frontend/src/types/ProjectTypes'
 
-const convertToDFA = (project: FSAProjectGraph): FSAProjectGraph => {
-  return reorderStates(convertNFAtoDFA(reorderStates(project)))
+// Required because we can't do `as const` to the imported JSON.
+// Means we don't need `as FSAProjectGraph` everywhere
+type LooseFSA = Omit<FSAProjectGraph, 'projectType'> & {projectType: string}
+
+const convertToDFA = (project: LooseFSA): FSAProjectGraph => {
+  return reorderStates(convertNFAtoDFA(reorderStates(project as FSAProjectGraph)))
 }
 
 describe('Check to ensure NFA graph is valid before conversion begins', () => {
   test('Graph should not be processed for conversion if there are no final states', () => {
     expect(() => {
-      convertToDFA(convertFinalNotPresent as FSAProjectGraph)
+      convertToDFA(convertFinalNotPresent)
     }).toThrow('Error: Graph is not suitable for conversion. Please ensure that at least one final state is declared.')
   })
 
   test('Graph should not be processed for conversion if there are no initial states', () => {
     expect(() => {
-      convertToDFA(convertInitialNotPresent as FSAProjectGraph)
+      convertToDFA(convertInitialNotPresent)
     }).toThrow('Error: Graph is not suitable for conversion. Please ensure that an initial state is declared.')
   })
 
   test('Graph should not be processed for conversion if there are no states or transitions', () => {
     expect(() => {
-      convertToDFA(convertNoStatesOrTransitionsPresent as FSAProjectGraph)
+      convertToDFA(convertNoStatesOrTransitionsPresent)
     }).toThrow('Error: Graph is not suitable for conversion. Please ensure you have both states and transitions present.')
   })
 
   test('Graph should not be processed for conversion if there are no reachable final states', () => {
     expect(() => {
-      convertToDFA(convertFinalNotReachable as FSAProjectGraph)
+      convertToDFA(convertFinalNotReachable)
     }).toThrow('Error: Graph is not suitable for conversion. Please ensure your final state is able to be reached by the initial state.')
   })
 })
 
 describe('Check to ensure DFA graph is displayed as expected', () => {
+  const expectDFA = (nfa: LooseFSA, dfa: LooseFSA) => {
+    const graph = convertToDFA(nfa)
+    // Only compare fields that are in FSAProjectGraph
+    expect(graph.initialState).toEqual(dfa.initialState)
+    expect(graph.states).toEqual(dfa.states)
+    expect(graph.transitions).toEqual(dfa.transitions)
+  }
   test('Graph should be converted correctly to DFA under simple conditions (1 symbol)', () => {
-    const graph = convertToDFA(convertSimpleConversion as FSAProjectGraph)
-    expect(graph).toEqual(convertSimpleConversionDFA)
+    expectDFA(convertSimpleConversion, convertSimpleConversionDFA)
   })
   test('Graph should be converted correctly to DFA when initial state is not at the start', () => {
-    const graph = convertToDFA(convertInitialNotAtStart as FSAProjectGraph)
-    expect(graph).toEqual(convertInitialNotAtStartDFA)
+    expectDFA(convertInitialNotAtStart, convertInitialNotAtStartDFA)
   })
   test('Graph should be converted correctly to DFA with multiple final states', () => {
-    const graph = convertToDFA(convertMultipleFinal as FSAProjectGraph)
-    expect(graph).toEqual(convertMultipleFinalDFA)
+    expectDFA(convertMultipleFinal, convertMultipleFinalDFA)
   })
   test('Graph should be converted correctly to DFA under harder conditions (2 symbols)', () => {
-    const graph = convertToDFA(convertHarderConversion as FSAProjectGraph)
-    expect(graph).toEqual(convertHarderConversionDFA)
+    expectDFA(convertHarderConversion, convertHarderConversionDFA)
   })
   test('Graph should use a single trap state instead of multiple when converted to a DFA', () => {
-    const graph = convertToDFA(convertSingleTrapState as FSAProjectGraph)
-    expect(graph).toEqual(convertSingleTrapStateDFA)
+    expectDFA(convertSingleTrapState, convertSingleTrapStateDFA)
   })
 })
