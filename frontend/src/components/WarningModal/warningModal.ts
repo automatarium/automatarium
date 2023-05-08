e9e5dfd9d368638d3e2462d1bf3c3314e523df6d
LvandoApps <lachlanvdk55@gmail.com>
2023-05-08 15:05:43 +1000

    Warning Modal styling changed

--- frontend/src/components/WarningModal/warningModal.ts
+++ frontend/src/components/WarningModal/warningModal.ts
@@ -2,22 +2,23 @@ import './warningModal.css'
 
 export function showWarningModal(message: string) {
     // Create the necessary DOM elements
-    console.log(message)
     const modal = document.createElement("div");
     modal.classList.add("modal");
 
     const content = document.createElement("div");
     content.classList.add("modal-content");
 
+    const title = document.createElement("h2");
+    title.textContent = "Warning";
+
     const messageElem = document.createElement("p");
     messageElem.textContent = message;
-    // Set text color to black, initially white for some reason
-    messageElem.style.color = "black";
 
     const okButton = document.createElement("button");
     okButton.textContent = "OK";
 
     // Add the elements to the DOM
+    content.appendChild(title);
     content.appendChild(messageElem);
     content.appendChild(okButton);
     modal.appendChild(content);
@@ -29,7 +30,9 @@ export function showWarningModal(message: string) {
     });
 
     // Apply styles to the modal and its content
-    modal.style.display = "block";
+    modal.style.display = "flex";
+    modal.style.alignItems = "center";
+    modal.style.justifyContent = "center";
     modal.style.position = "fixed";
     modal.style.top = "0";
     modal.style.left = "0";
@@ -38,12 +41,33 @@ export function showWarningModal(message: string) {
     modal.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     modal.style.zIndex = "9999";
 
-    content.style.position = "absolute";
-    content.style.top = "50%";
-    content.style.left = "50%";
-    content.style.transform = "translate(-50%, -50%)";
+    content.style.display = "flex";
+    content.style.flexDirection = "column";
+    content.style.alignItems = "center";
     content.style.backgroundColor = "#fff";
     content.style.padding = "20px";
     content.style.borderRadius = "5px";
     content.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.5)";
-}  
+
+    title.style.color = "#f44336";
+    title.style.fontSize = "24px";
+    title.style.marginBottom = "20px";
+
+    messageElem.style.color = "#000";
+    messageElem.style.marginBottom = "20px";
+
+    okButton.style.padding = "10px";
+    okButton.style.backgroundColor = "#f44336";
+    okButton.style.color = "#fff";
+    okButton.style.border = "none";
+    okButton.style.borderRadius = "5px";
+    okButton.style.cursor = "pointer";
+
+    okButton.addEventListener("mouseenter", () => {
+        okButton.style.backgroundColor = "#d32f2f";
+    });
+
+    okButton.addEventListener("mouseleave", () => {
+        okButton.style.backgroundColor = "#f44336";
+    });
+}
