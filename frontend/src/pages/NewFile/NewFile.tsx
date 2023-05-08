1ae6371e43bdf282a8467dcd0d234c88878ef689
Jake Leahy <jake@leahy.dev>
2023-05-08 19:07:01 +1000

    Only accept `.jff` and `.json` files in importer (#374)

--- frontend/src/pages/NewFile/NewFile.tsx
+++ frontend/src/pages/NewFile/NewFile.tsx
@@ -60,6 +60,7 @@ const NewFile = () => {
     // Prompt user for file input
     const input = document.createElement('input')
     input.type = 'file'
+    input.accept = '.jff,.json'
     input.onchange = () => {
       // Read file data
       const reader = new FileReader()
