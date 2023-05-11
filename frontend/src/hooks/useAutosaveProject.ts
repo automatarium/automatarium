0c0929c44686ae240b39ba334255abec9a6b3980
Jake Leahy <jake@leahy.dev>
2023-05-11 13:53:35 +1000

    Don't save blank projects (#333)
    We check that they have added a comment, state, or transition since I felt those would be the things that someone would add

--- frontend/src/hooks/useAutosaveProject.ts
+++ frontend/src/hooks/useAutosaveProject.ts
@@ -5,6 +5,11 @@ import dayjs from 'dayjs'
 
 const SAVE_INTERVAL = 5 * 1000
 
+/**
+ * Use this to save the project on an interval. The project will only save if there are changes
+ * and there are items in the project
+ * @see SAVE_INTERVAL
+ */
 const useAutosaveProject = () => {
   const upsertProject = useProjectsStore(s => s.upsertProject)
   const lastChangeDate = useProjectStore(s => s.lastChangeDate)
@@ -13,9 +18,11 @@ const useAutosaveProject = () => {
 
   useEffect(() => {
     const timer = setInterval(() => {
-      const currentProject = useProjectStore.getState().project
-      if (!lastSaveDate || dayjs(lastChangeDate).isAfter(lastSaveDate)) {
-        const toSave = { ...currentProject, meta: { ...currentProject.meta, dateEdited: new Date().getTime() } }
+      const currP = useProjectStore.getState().project
+      const totalItems = currP.comments.length + currP.states.length + currP.transitions.length
+      // Only save if there has been a change and there is something in the project
+      if ((!lastSaveDate || dayjs(lastChangeDate).isAfter(lastSaveDate)) && totalItems > 0) {
+        const toSave = { ...currP, meta: { ...currP.meta, dateEdited: new Date().getTime() } }
         upsertProject(toSave)
         setLastSaveDate(new Date().getTime())
       }
