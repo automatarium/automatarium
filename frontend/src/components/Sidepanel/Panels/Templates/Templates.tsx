175e3f6ad0ff306a1915256c6dd08c329ea0c1e9
Ethan Wang <s3909422@student.rmit.edu.au>
2023-08-08 18:13:54 +1000

    Stray remove reference to projectId

--- frontend/src/components/Sidepanel/Panels/Templates/Templates.tsx
+++ frontend/src/components/Sidepanel/Panels/Templates/Templates.tsx
@@ -100,7 +100,6 @@ const Templates = () => {
                 key={temp._id}
                 name={temp.name}
                 date={dayjs(temp.date)}
-                projectId={temp._id}
                 width={TEMPLATE_THUMBNAIL_WIDTH}
                 $istemplate={true}
                 showKebab={false}
