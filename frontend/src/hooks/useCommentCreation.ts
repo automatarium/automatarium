968d692b213d366d8b618a568fa3992b2faf6ff0
Ethan Wang <s3909422@student.rmit.edu.au>
2023-08-22 20:38:39 +1000

    Add event listeners for comment tool

--- frontend/src/hooks/useCommentCreation.ts
+++ frontend/src/hooks/useCommentCreation.ts
@@ -1,15 +1,22 @@
 import { useEvent } from '/src/hooks'
 import { dispatchCustomEvent } from '/src/util/events'
 import { useToolStore } from '/src/stores'
+import { useCallback } from 'react'
 
 const useCommentCreation = () => {
   const tool = useToolStore(s => s.tool)
 
-  useEvent('svg:mouseup', e => {
+  const handleCommentMouseUp = useCallback((e) => {
     if (tool === 'comment' && e.detail.didTargetSVG && e.detail.originalEvent.button === 0) {
       window.setTimeout(() => dispatchCustomEvent('editComment', { x: e.detail.originalEvent.clientX, y: e.detail.originalEvent.clientY }), 100)
     }
   }, [tool])
+
+  useEvent('svg:mouseup', handleCommentMouseUp)
+  useEvent('state:mouseup', handleCommentMouseUp)
+  useEvent('transition:mouseup', handleCommentMouseUp)
+  useEvent('comment:mouseup', handleCommentMouseUp)
+  useEvent('edge:mouseup', handleCommentMouseUp)
 }
 
 export default useCommentCreation
