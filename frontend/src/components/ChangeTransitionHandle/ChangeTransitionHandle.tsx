99322c83449069e7209c8ed7235bdfbfb6efaedc
Ethan Wang <s3909422@student.rmit.edu.au>
2023-09-18 19:06:58 +1000

    Add listeners to handlebars

--- frontend/src/components/ChangeTransitionHandle/ChangeTransitionHandle.tsx
+++ frontend/src/components/ChangeTransitionHandle/ChangeTransitionHandle.tsx
@@ -4,7 +4,7 @@ import { BOX_HANDLE_SIZE } from '/src/config/rendering'
 import { CustomEvents } from '/src/hooks/useEvent'
 import { dispatchCustomEvent } from '/src/util/events'
 
-type TransitionChangeHandle = {
+type TransitionChangeHandleProps = {
   cx: number
   cy: number
   transitionId: number
@@ -13,16 +13,30 @@ type TransitionChangeHandle = {
   isStart: boolean
 }
 
-const ChangeTransitionHandle = ({ cx, cy, transitionId, fromId, toId, isStart, ...props }: TransitionChangeHandle) => {
+const ChangeTransitionHandle = ({ cx, cy, transitionId, fromId, toId, isStart, ...props }: TransitionChangeHandleProps) => {
   // console.log(`${transitionId} ${fromId} ${toId} ${isStart}`)
+
+  const handleEvent = (eventName: keyof CustomEvents) => (e: MouseEvent) => {
+    dispatchCustomEvent(eventName, {
+      originalEvent: e,
+      transitionInfo: {
+        transitionId,
+        fromId,
+        toId,
+        isStart
+      }
+    })
+  }
 
   // Middle-ise co-ordinates
   const x = cx - BOX_HANDLE_SIZE / 2
   const y = cy - BOX_HANDLE_SIZE / 2
 
   return <g transform={`translate(${x}, ${y})`}
+            onMouseDown={handleEvent('transitionhandle:mousedown')}
+            onMouseUp={handleEvent('transitionhandle:mouseup')}
             {...props}>
-    <rect width={BOX_HANDLE_SIZE} height={BOX_HANDLE_SIZE} style={handleStyle} z={99999} />
+    <rect width={BOX_HANDLE_SIZE} height={BOX_HANDLE_SIZE} style={handleStyle} />
   </g>
 }
 
