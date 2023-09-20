302bf9f8627ac2b30360154d0d070784656cf14c
Ethan Wang <s3909422@student.rmit.edu.au>
2023-09-20 18:32:45 +1000

    Fixes video iframe size

--- frontend/src/pages/Tutorials/components/YoutubeTutorialEmbed/YoutubeTutorialEmbed.tsx
+++ frontend/src/pages/Tutorials/components/YoutubeTutorialEmbed/YoutubeTutorialEmbed.tsx
@@ -1,14 +1,32 @@
+import { useLayoutEffect, useState } from 'react'
+
 const EmbeddedVideo = ({ link }) => {
+  const [aspectRatios, setAspectRatio] = useState([560, 315])
+
   const parseId = (link: string) => {
     const tokens = link.split('/') ?? null
     return tokens.find(tk => tk === 'youtu.be') === undefined ? null : tokens[tokens.length - 1]
   }
 
+  const updateRatios = () => {
+    const usableWidth = window.innerWidth > 1200 ? 1136 : window.innerWidth - 64
+    const videoHeight = usableWidth * (315 / 560)
+    setAspectRatio([usableWidth, videoHeight])
+  }
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', updateRatios)
+
+    return () => {
+      window.removeEventListener('resize', updateRatios)
+    }
+  }, [])
+
   const vidId = parseId(link)
 
   return vidId && <iframe
-      width={560}
-      height={315}
+      width={aspectRatios[0]}
+      height={aspectRatios[1]}
       src={`https://www.youtube.com/embed/${vidId}`}
       allow="accelerometer; clipboard-write; encrypted-media"
       allowFullScreen
