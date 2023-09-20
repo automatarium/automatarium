88a7f9da692dd074093867fe4136dada94bd5e76
Ethan Wang <s3909422@student.rmit.edu.au>
2023-09-20 17:07:34 +1000

    Adds embedded youtube video

--- frontend/src/pages/Tutorials/TutorialsVideo.tsx
+++ frontend/src/pages/Tutorials/TutorialsVideo.tsx
@@ -1,6 +1,7 @@
 import { useSearchParams } from 'react-router-dom'
 import { Button, Header, Main } from '/src/components'
 import { TutorialLeaf } from './TutorialsPage'
+import EmbeddedVideo from './YoutubeTutorialEmbed'
 
 type TutorialVideoProps = {
   pageInfo: TutorialLeaf
@@ -11,11 +12,19 @@ const TutorialsVideo = ({ pageInfo, pagePath }: TutorialVideoProps) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, setSearchParams] = useSearchParams()
 
+  const parseId = (link: string) => {
+    const tokens = link.split('/') ?? null
+    return tokens.find(tk => tk === 'youtu.be') === undefined ? null : tokens[tokens.length - 1]
+  }
+
+  const vidId = parseId(pageInfo.link)
+
   return <Main wide>
     <Header linkTo="/" />
     <h1>{pageInfo.title}</h1>
     {pagePath.length > 0 && <><Button onClick={() => setSearchParams(pagePath.slice(0, -1).join('&'))}>Go back</Button><br/></>}
     {<p>{pageInfo.description}</p>}
+    {vidId && <EmbeddedVideo id={vidId}/>}
   </Main>
 }
 
