3f1bcd70ec0e2b0aa1f1ff258dccd182359cd3ad
Ethan Wang <s3909422@student.rmit.edu.au>
2023-09-23 01:33:41 +1000

    Add event handling for cursor drag

--- frontend/src/components/ChangeTransitionHandle/ChangeTransitionHandle.tsx
+++ frontend/src/components/ChangeTransitionHandle/ChangeTransitionHandle.tsx
@@ -3,7 +3,9 @@ import { handleStyle } from './changeTransitionHandleStyle'
 import { BOX_HANDLE_SIZE } from '/src/config/rendering'
 import { dispatchCustomEvent } from '/src/util/events'
 import { Coordinate } from '/src/types/ProjectTypes'
-import { useProjectStore } from '/src/stores'
+import { useProjectStore, useToolStore, useViewStore } from '/src/stores'
+import { useEvent } from '/src/hooks'
+import TransitionSet from '../TransitionSet/TransitionSet'
 
 type TransitionChangeHandleProps = {
   edges: Coordinate[]
@@ -13,65 +15,15 @@ type TransitionChangeHandleProps = {
 type RectCoords = { start: Coordinate, end: Coordinate }
 
 const ChangeTransitionHandlebars = ({ edges, selectedTransitions, ...props }: TransitionChangeHandleProps) => {
+  const tool = useToolStore(s => s.tool)
+  const screenToViewSpace = useViewStore(s => s.screenToViewSpace)
+
   const [isSameEdge, setIsSameEdge] = useState(true)
   const [from, setFrom] = useState<number>()
   const [to, setTo] = useState<number>()
-
-  useEffect(() => {
-    const { transitions } = useProjectStore.getState()?.project ?? {}
-    const transitionsScope = transitions.filter(t => selectedTransitions.includes(t.id))
-    setIsSameEdge(transitions.every(t => t.from === transitionsScope[0].from && t.to === transitionsScope[0].to))
-    setFrom(transitionsScope[0].from)
-    setTo(transitionsScope[0].to)
-  }, [selectedTransitions])
-
-  const handleStartMouseUp = (e: MouseEvent) => {
-    dispatchCustomEvent('transitionhandle:mouseup', {
-      originalEvent: e,
-      transitionInfo: {
-        transitionIds: selectedTransitions,
-        fromId: from,
-        toId: to,
-        isStart: true
-      }
-    })
-  }
-
-  const handleStartMouseDown = (e: MouseEvent) => {
-    dispatchCustomEvent('transitionhandle:mousedown', {
-      originalEvent: e,
-      transitionInfo: {
-        transitionIds: selectedTransitions,
-        fromId: from,
-        toId: to,
-        isStart: true
-      }
-    })
-  }
-
-  const handleEndMouseUp = (e: MouseEvent) => {
-    dispatchCustomEvent('transitionhandle:mouseup', {
-      originalEvent: e,
-      transitionInfo: {
-        transitionIds: selectedTransitions,
-        fromId: from,
-        toId: to,
-        isStart: false
-      }
-    })
-  }
-
-  const handleEndMouseDown = (e: MouseEvent) => {
-    dispatchCustomEvent('transitionhandle:mousedown', {
-      originalEvent: e,
-      transitionInfo: {
-        transitionIds: selectedTransitions,
-        fromId: from,
-        toId: to,
-        isStart: false
-      }
-    })
-  }
+  const [isDragging, setIsDragging] = useState(false)
+  const [dragStart, setDragStart] = useState<Coordinate>(null)
+  const [dragCursor, setDragCursor] = useState<[number, number]>(null)
 
   // Middle-ise co-ordinates
   const t: RectCoords = {
@@ -85,22 +37,97 @@ const ChangeTransitionHandlebars = ({ edges, selectedTransitions, ...props }: Tr
     }
   }
 
-  return isSameEdge && <g {...props}>
-    <rect
-      transform={`translate(${t.start.x}, ${t.start.y})`}
-      width={BOX_HANDLE_SIZE}
-      height={BOX_HANDLE_SIZE}
-      style={handleStyle}
-      onMouseUp={handleStartMouseUp}
-      onMouseDown={handleStartMouseDown} />
-    <rect
-      transform={`translate(${t.end.x}, ${t.end.y})`}
-      width={BOX_HANDLE_SIZE}
-      height={BOX_HANDLE_SIZE}
-      style={handleStyle}
-      onMouseUp={handleEndMouseUp}
-      onMouseDown={handleEndMouseDown} />
-  </g>
+  useEffect(() => {
+    const { transitions } = useProjectStore.getState()?.project ?? {}
+    const transitionsScope = transitions.filter(t => selectedTransitions.includes(t.id))
+    setIsSameEdge(transitions.every(t => t.from === transitionsScope[0].from && t.to === transitionsScope[0].to))
+    setFrom(transitionsScope[0].from)
+    setTo(transitionsScope[0].to)
+  }, [selectedTransitions])
+
+  const handleStartMouseDown = (e: MouseEvent) => {
+    setDragStart(t.start)
+    if (isSameEdge) {
+      setIsDragging(true)
+      dispatchCustomEvent('transitionhandle:mousedown', {
+        originalEvent: e,
+        transitionInfo: {
+          transitionIds: selectedTransitions,
+          fromId: from,
+          toId: to,
+          isStart: true
+        }
+      })
+    }
+  }
+
+  useEvent('transitionhandle:mousedown', e => {
+    setDragCursor(screenToViewSpace(e.detail.originalEvent.clientX, e.detail.originalEvent.clientY))
+  })
+
+  useEvent('state:mouseup', e => {
+    if (tool === 'cursor' && e.detail.originalEvent.button === 0 && isDragging) {
+      console.log(e.detail)
+      setIsDragging(false)
+      // dispatchCustomEvent('transitionhandle:mouseup', {
+      //   originalEvent: e,
+      //   transitionInfo: {
+      //     transitionIds: selectedTransitions,
+      //     fromId: from,
+      //     toId: to,
+      //     isStart: false
+      //   }
+      // })
+    }
+    setDragStart(null)
+    setDragCursor(null)
+  }, [tool, isDragging])
+
+  const handleEndMouseDown = (e: MouseEvent) => {
+    setDragStart(t.end)
+    if (isSameEdge) {
+      setIsDragging(true)
+      dispatchCustomEvent('transitionhandle:mousedown', {
+        originalEvent: e,
+        transitionInfo: {
+          transitionIds: selectedTransitions,
+          fromId: from,
+          toId: to,
+          isStart: false
+        }
+      })
+    }
+  }
+
+  useEvent('svg:mousemove', e => {
+    setDragCursor([e.detail.viewX, e.detail.viewY])
+  }, [tool, isDragging, dragStart])
+
+  return <>
+    <g {...props}>
+      <rect
+        transform={`translate(${t.start.x}, ${t.start.y})`}
+        width={BOX_HANDLE_SIZE}
+        height={BOX_HANDLE_SIZE}
+        style={handleStyle}
+        onMouseDown={handleStartMouseDown} />
+      <rect
+        transform={`translate(${t.end.x}, ${t.end.y})`}
+        width={BOX_HANDLE_SIZE}
+        height={BOX_HANDLE_SIZE}
+        style={handleStyle}
+        onMouseDown={handleEndMouseDown} />
+    </g>
+    {dragStart && dragCursor && <TransitionSet.Transition
+      fullWidth
+      suppressEvents
+      from={dragStart}
+      to={{ x: dragCursor[0], y: dragCursor[1] }}
+      projectType='FSA'
+      id={-1}
+      transitions={[]}
+    />}
+  </>
 }
 
 export default ChangeTransitionHandlebars
