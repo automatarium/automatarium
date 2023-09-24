9771c2b7940596855140f297999ac390d0012b4f
Ethan Wang <82514325+anioncat@users.noreply.github.com>
2023-09-24 19:49:30 +1000

    Transition dragging (#435)
    Closes #363.
    
    Adds smallish circles to the end of transitions that can be dragged to a
    different state.
    
    
    ![image](https://github.com/automatarium/automatarium/assets/82514325/f5e9506d-ef0e-4e34-92df-d09064ca17ea)
    
    Uses the `useTransitionCreation` hook to render a guide on where the
    selected transitions will be reallocated to. Only the selected
    transitions on the same edge will be moved.

--- frontend/src/components/TransitionSet/TransitionSet.tsx
+++ frontend/src/components/TransitionSet/TransitionSet.tsx
@@ -1,11 +1,12 @@
 import { MouseEvent, useContext } from 'react'
 import { useProjectStore } from '../../stores'
+import ChangeTransitionHandlebars from '../ChangeTransitionHandle/ChangeTransitionHandle'
+import { ghostStyles, pathSelectedClass, pathStyles } from './transitionSetStyle'
+import { REFLEXIVE_X_OFFSET, REFLEXIVE_Y_OFFSET, STATE_CIRCLE_RADIUS, TRANSITION_SEPERATION } from '/src/config/rendering'
 import { MarkerContext } from '/src/providers'
-import { STATE_CIRCLE_RADIUS, TRANSITION_SEPERATION, REFLEXIVE_Y_OFFSET, REFLEXIVE_X_OFFSET } from '/src/config/rendering'
-import { movePointTowards, lerpPoints, size } from '/src/util/points'
-import { dispatchCustomEvent } from '/src/util/events'
 import { useSelectionStore } from '/src/stores'
-import { pathStyles, pathSelectedClass, ghostStyles } from './transitionSetStyle'
+import { dispatchCustomEvent } from '/src/util/events'
+import { lerpPoints, movePointTowards, size } from '/src/util/points'
 import { PositionedTransition } from '/src/util/states'
 import { assertType, Coordinate, PDAAutomataTransition, ProjectType, TMAutomataTransition } from '/src/types/ProjectTypes'
 import { splitCharsWithOr } from '/src/util/orOperators'
@@ -234,6 +235,14 @@ const Transition = ({
       onDoubleClick={handleEdgeDoubleClick}
       strokeWidth={20}
     />}
+
+    {/* Handles to drag the edge */}
+    {setSelected && <ChangeTransitionHandlebars
+        edges={edges}
+        selectedTransitions={selectedTransitions}
+        isReflexive={isReflexive}
+      />
+    }
 
     <text
       {...midPoint}
