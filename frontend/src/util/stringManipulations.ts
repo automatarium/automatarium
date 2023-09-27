03cab82358a7642b2c2ed27c03fbd52b1b3897e3
S3865562 <S3865562@student.rmit.edu.au>
2023-09-27 13:52:05 +1000

    Fix ordering of string manipulation issue

--- frontend/src/util/stringManipulations.ts
+++ frontend/src/util/stringManipulations.ts
@@ -33,53 +33,51 @@ export const splitCharsWithOr = (text: string, orOperator: string): string => {
   let joinStr = ' '
   if (orOperator !== ' ') { joinStr = ` ${orOperator} ` }
 
-  // Don't insert OR symbols inside ranges or between NOT operator (!) and following character 
-  const regex = /(\[.*?])|(!+[a-zA-Z])|([a-zA-Z])/g
-  let matches = Array.from(text.matchAll(regex))
-  let parts = matches.map(match => match[0])
-
-  return parts.join(joinStr);
+  const parts = text.match(/(\[.*?])|(![^!])|([^!])/g)
+  return parts ? parts.join(joinStr) : ''
 }
 
 export const removeWhitespace = (input: string): string => {
-  return input.replace(/\s+/g, '');
+  return input.replace(/\s+/g, '')
 }
 
 export const removeOrOperators = (input: string, orOperator: string): string => {
   for (const op of possibleOrOperators(orOperator)) {
-    input = input.split(op).join('');
+    input = input.split(op).join('')
   }
-  return input;
+  return input
 }
 
 export const removeDuplicateChars = (input: string): string => {
-  return Array.from(new Set(input.split(''))).join('');
+  return Array.from(new Set(input.split(''))).join('')
 }
 
 export const extractRanges = (input: string): [string, string[]] => {
-  const ranges = input.match(/\[(.*?)]/g) || [];
-  input = input.replace(/\[(.*?)]/g, '');
-  return [input, ranges];
+  const ranges = input.match(/\[(.*?)]/g) || []
+  input = input.replace(/\[(.*?)]/g, '')
+  return [input, ranges]
 }
 
 // Exclusions are denoted with ! followed by the character to exclude
 export const extractAndRemoveExclusions = (input: string): [string, string] => {
-  const exclusionMatches = input.match(/!+([^!])/g) || [];
+  const exclusionMatches = input.match(/!+([^!])/g) || []
   for (const match of exclusionMatches) {
-    input = input.replace(match, '');
+    input = input.replace(match, '')
   }
-  input = input.replace(/!+/g, '');
-  const uniqueExclusions = Array.from(new Set(exclusionMatches.map(match => '!' + match[match.length - 1]))).join('');
-  return [input, uniqueExclusions];
+  input = input.replace(/!+/g, '')
+  const uniqueExclusions = Array.from(new Set(exclusionMatches.map(match => '!' + match[match.length - 1]))).join('')
+  return [input, uniqueExclusions]
 }
 
-
+// Extracts ranges and exclusion pairs to readd to return value
+// Removes whitespace, or operators, and duplicates
 // If the input is 'a[w-z]a!!b', the output will be 'a!b[w-z]'
 export const formatInput = (input: string, orOperator: string): string => {
-  input = removeWhitespace(input);
-  input = removeOrOperators(input, orOperator);
+  const [inputWithoutRanges, ranges] = extractRanges(input)
+  const [inputWithoutExclusions, uniqueExclusions] = extractAndRemoveExclusions(inputWithoutRanges)
+  input = removeWhitespace(inputWithoutExclusions)
+  input = removeOrOperators(input, orOperator)
   input = removeDuplicateChars(input)
-  const [inputWithoutRanges, ranges] = extractRanges(input);
-  const [finalInput, uniqueExclusions] = extractAndRemoveExclusions(inputWithoutRanges);
-  return `${finalInput}${uniqueExclusions}${ranges.join('')}`;
+
+  return `${input}${uniqueExclusions}${ranges.join('')}`
 }
