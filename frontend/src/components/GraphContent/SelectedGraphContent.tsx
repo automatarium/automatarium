3f458e4a845e672e2e2b077ab8c94b491de3599e
Ethan Wang <s3909422@student.rmit.edu.au>
2023-10-02 18:11:46 +1100

    Add comments as part of templates render

--- frontend/src/components/GraphContent/SelectedGraphContent.tsx
+++ frontend/src/components/GraphContent/SelectedGraphContent.tsx
@@ -1,17 +1,20 @@
+import { useMemo } from 'react'
+import { getGroupedTransitions } from './utils'
+import CommentRect from '/src/components/CommentRect/CommentRect'
 import StateCircle from '/src/components/StateCircle/StateCircle'
 import TransitionSet from '/src/components/TransitionSet/TransitionSet'
-import { getGroupedTransitions } from './utils'
 import { useProjectStore, useSelectionStore } from '/src/stores'
-import { useMemo } from 'react'
 
 const SelectedGraphContent = () => {
   const project = useProjectStore(s => s.project)
-  const { transitions, states } = project ?? {}
+  const { transitions, states, comments } = project ?? {}
   const selectedTransitionIds = useSelectionStore(s => s.selectedTransitions)
   const selectedStateIds = useSelectionStore(s => s.selectedStates)
+  const selectedCommentsIds = useSelectionStore(s => s.selectedComments)
 
   const selectedTransitions = useMemo(() => transitions.filter(t => selectedTransitionIds.includes(t.id)), [transitions])
   const selectedStates = useMemo(() => states.filter(s => selectedStateIds.includes(s.id)), [states])
+  const selectedComments = useMemo(() => comments.filter(c => selectedCommentsIds.includes(c.id)), [comments, selectedCommentsIds])
 
   const locatedTransitions = useMemo(() =>
     getGroupedTransitions(selectedTransitions, selectedStates),
@@ -19,14 +22,14 @@ const SelectedGraphContent = () => {
 
   return <>
     {/* Render all sets of edges */}
-    {locatedTransitions.map((transitions, i) => <TransitionSet
+    {!!locatedTransitions && locatedTransitions.map((transitions, i) => <TransitionSet
       transitions={transitions}
       isTemplate={true}
       key={i}
     />)}
 
     {/* Render all states */}
-    {selectedStates.map(s => <StateCircle
+    {!!selectedStates && selectedStates.map(s => <StateCircle
       key={s.id}
       id={s.id}
       name={s.name}
@@ -36,6 +39,15 @@ const SelectedGraphContent = () => {
       isFinal={s.isFinal}
       selected={false}
       stepped={false}
+    />)}
+
+    {/* Render all comments */}
+    {!!selectedComments && selectedComments.map(c => <CommentRect
+      key={c.id}
+      id={c.id}
+      x={c.x}
+      y={c.y}
+      text={c.text}
     />)}
   </>
 }
