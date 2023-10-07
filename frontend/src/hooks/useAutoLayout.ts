d3fc463759468279c0c4f52a89576bd9c9dd9c98
Ethan Wang <s3909422@student.rmit.edu.au>
2023-10-07 17:41:51 +1100

    Choose algorithm via dictionary of names

--- frontend/src/hooks/useAutoLayout.ts
+++ frontend/src/hooks/useAutoLayout.ts
@@ -1,8 +1,15 @@
 import GemLayoutAlgorithm from '/src/util/Layouts/GemLayoutAlgorithm'
 import { ProjectGraph } from '/src/types/ProjectTypes'
 
-const useAutoLayout = (graph: ProjectGraph, algorithm: (graph: ProjectGraph) => ProjectGraph = GemLayoutAlgorithm) => {
-  return algorithm(graph)
+type LayoutName = 'gem'
+type LayoutAlgorithm = (graph: ProjectGraph) => ProjectGraph
+
+const useAutoLayout = (graph: ProjectGraph, algorithm: LayoutName = 'gem') => {
+  const algorithms = new Map<string, LayoutAlgorithm>([
+    ['gem', GemLayoutAlgorithm]
+  ])
+  if (!algorithms.has(algorithm)) { return null }
+  return algorithms.get(algorithm)(graph)
 }
 
 export default useAutoLayout
