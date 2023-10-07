9358d9c5feed08f580846e2490d45bcc23da91dd
Ethan Wang <s3909422@student.rmit.edu.au>
2023-10-08 00:45:15 +1100

    Begin working on graphviz algorithm

---
+++ frontend/src/util/Layouts/GraphvizLayoutAlgorithm.ts
@@ -0,0 +1,63 @@
+/**
+ * Implements the algorithm described in [this paper](https://www.graphviz.org/documentation/TSE93.pdf)
+ * 'A Technique for Drawing Directed Graphs' by Gansner E.R. et. al.
+ */
+import { Record, Records } from './types'
+import { STATE_CIRCLE_RADIUS } from '/src/config/rendering'
+import { ProjectGraph } from '/src/types/ProjectTypes'
+
+type TKey = [number, number]
+
+const GraphvizLayoutAlgorithm = (graph: ProjectGraph) => {
+  const graphClone = structuredClone(graph)
+  const cloneStates = graphClone.states
+
+  // Make graph acyclic
+  // Ignore reflexive transitions
+  const noReflex = graphClone.transitions.filter(t => !(t.from === t.to))
+  // Merge edges and assign weight according to number of transitions
+  const edges = new Map<[number, number], number>()
+  for (const t of noReflex) {
+    const transitionKey = [t.from, t.to] as TKey
+    if (edges.has(transitionKey)) {
+      edges.set(transitionKey, edges.get(transitionKey) + 1)
+    } else {
+      edges.set(transitionKey, 1)
+    }
+  }
+
+  // Size of bounding box of a node
+  const nodeB = [STATE_CIRCLE_RADIUS, STATE_CIRCLE_RADIUS]
+  // Minimum horizontal separation
+  const nodeSep = 5
+  // Minimum vertical separation
+  const rankSep = 5
+
+  console.log(`${nodeB}, ${nodeSep}, ${rankSep}`)
+
+  const records = {} as Records
+
+  cloneStates.forEach(v => {
+    const adjacentStateIds = []
+    for (const t of graphClone.transitions) {
+      if (t.from === v.id && !adjacentStateIds.includes(t.to)) {
+        adjacentStateIds.push(t.to)
+      }
+      if (t.to === v.id && !adjacentStateIds.includes(t.from)) {
+        adjacentStateIds.push(t.from)
+      }
+    }
+    const r = {
+      point: { x: v.x, y: v.y },
+      omega: 1,
+      adjacentList: adjacentStateIds
+    } as Record
+    records[v.id] = r
+  })
+
+  // rank
+  // ordering
+  // position
+}
+
+export default GraphvizLayoutAlgorithm
