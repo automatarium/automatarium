1c6d257ff8be89b81b1702cb4914826863cc0d32
Ethan Wang <s3909422@student.rmit.edu.au>
2023-10-23 01:57:19 +1100

    Add cycle check in tests

--- packages/simulation/tests/testDAGConversion.test.ts
+++ packages/simulation/tests/testDAGConversion.test.ts
@@ -18,9 +18,59 @@ import simpleFutureSolution from './graphs/convertToDAGFutureCycleSolution.json'
 type Graph = Omit<ProjectGraph, 'projectType'> & {projectType: string}
 const convert = (g: Graph) => convertToDAG(g as ProjectGraph)[0]
 
+/** I suspect some nodes may become unreachable but this doesn't check for that.
+ *  It will cost a lot to check for cycles within enclaves etc.
+ */
+const hasCycles = (graph: ProjectGraph) : boolean => {
+  const getInitial = (graph: ProjectGraph) : number => {
+    if (graph.initialState) return graph.initialState
+    // let's start from zero!
+    let sStartFromZero = 0
+    while (!graph.transitions.some(t => t.id === sStartFromZero)) {
+      sStartFromZero += 1
+    }
+    return sStartFromZero
+  }
+
+  const buildAdjacencyList = (graph: ProjectGraph) : Map<number, number[]> => {
+    const adjList = new Map<number, number[]>()
+    graph.transitions.forEach(t => {
+      if (adjList.has(t.from)) {
+        adjList.get(t.from).push(t.to)
+      } else {
+        adjList.set(t.from, [t.to])
+      }
+    })
+    return adjList
+  }
+
+  const adjList = buildAdjacencyList(graph)
+
+  const initialState = getInitial(graph)
+  const visited = []
+  const frontier = []
+  frontier.push(initialState)
+
+  while (frontier.length > 0) {
+    const current = frontier.pop()
+    const successors = adjList.get(current) ?? []
+    successors.forEach(s => {
+      if (visited.includes(s)) {
+        return true
+      }
+      if (!frontier.includes(s)) {
+        frontier.push(s)
+      }
+    })
+    visited.push(current)
+  }
+  return false
+}
+
 describe('Test that DAG conversion is correct', () => {
   const expectDAG = (initial: Graph, result: Graph) => {
     const graph = convert(initial)
+    expect(hasCycles(graph)).toBeFalse()
     expect(graph.initialState).toEqual(result.initialState)
     expect(graph.states).toEqual(result.states)
     expect(graph.transitions).toEqual(result.transitions)
