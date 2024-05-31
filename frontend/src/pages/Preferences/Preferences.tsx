f8d35870eae668c174fc70ca58c4c4e915486953
Kelvin Duong Ly <s3953996@student.rmit.edu.au>
2024-05-31 17:11:26 +1000

    reload page when save changes for preferences

--- frontend/src/pages/Preferences/Preferences.tsx
+++ frontend/src/pages/Preferences/Preferences.tsx
@@ -27,6 +27,7 @@ const Preferences = () => {
   const onSubmit = (values: Preferences) => {
     setPreferences(values)
     setIsOpen(false)
+    window.location.reload();
   }
 
   useEffect(() => {
