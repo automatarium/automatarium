62f7b99dabf6d0af21d10c8fe9a0448d4286fd93
Dib <79500311+tdib@users.noreply.github.com>
2024-07-27 20:01:57 +1000

    Rename original JFLAP translator file to be clearer

--- packages/jflap-translator/src/index.ts
+++ packages/jflap-translator/src/index.ts
@@ -1,2 +1,2 @@
-export { convertJFLAPXML, convertJFLAPProject } from './convertJFLAP'
+export { convertJFLAPXML, convertJFLAPProject } from './convertJFLAPToAutomatarium'
 export { convertAutomatariumToJFLAP } from './convertAutomatariumToJFLAP'
