811d3233c5d18b8fa2cce9d7f08043e89e0e1154
Dib <79500311+tdib@users.noreply.github.com>
2024-07-27 19:53:26 +1000

    Fix issue with empty PDA transitions not carrying all properties, lint project

--- packages/jflap-translator/src/convertAutomatariumToJFLAP.ts
+++ packages/jflap-translator/src/convertAutomatariumToJFLAP.ts
@@ -18,9 +18,9 @@ import {
 } from 'frontend/src/types/ProjectTypes'
 
 const PROJECT_TYPE_MAP: Record<ProjectType, string> = {
-  'FSA': 'fa',
-  'PDA': 'pda',
-  'TM': 'turing'
+  FSA: 'fa',
+  PDA: 'pda',
+  TM: 'turing'
 }
 
 const mapStates = (states: AutomataState[], initialStateId: number, statePrefix: string): JFLAPState[] => states.map((state) => ({
@@ -30,13 +30,53 @@ const mapStates = (states: AutomataState[], initialStateId: number, statePrefix:
   },
   x: { _text: state.x },
   y: { _text: state.y },
-  ...(state.label ? { label: { _text: state.label }} : {}),
+  ...(state.label ? { label: { _text: state.label } } : {}),
   ...(state.isFinal ? { final: {} } : {}),
   ...(state.id === initialStateId ? { initial: {} } : {})
 }))
 
-
 const mapTransitions = (transitions: AutomataTransition[], projectType: ProjectType): JFLAPTransition[] => {
+  /**
+   *
+   * @param transition The transition to add project specific attributes to
+   * @param currentRead A single character to apply the transformation to. If the transition is a range, this will be
+   * a single character from the range, otherwise will simply be the read symbol
+   * @param projectType One of "FSA", "PDA", or "TM". Used to determine the additional attributes
+   * @returns A JFLAP compatible transition with the project type specific attributes added
+   */
+  const addProjectSpecificAttributes = (transition: AutomataTransition, currentRead: string, projectType: ProjectType): JFLAPTransition => {
+    const baseTransition: BaseJFLAPTransition = {
+      from: { _text: transition.from },
+      to: { _text: transition.to },
+      read: currentRead ? { _text: currentRead } : {}
+    }
+    switch (projectType) {
+      // No extra properties needed for FSA
+      case 'FSA': {
+        return baseTransition
+      }
+
+      // Add pop and push properties to transition
+      case 'PDA': {
+        const pdaTransition = transition as PDAAutomataTransition
+        return {
+          ...baseTransition,
+          push: pdaTransition.push ? { _text: pdaTransition.push } : {},
+          pop: pdaTransition.pop ? { _text: pdaTransition.pop } : {}
+        }
+      }
+
+      // Add write and move properties to transition
+      case 'TM': {
+        const tmTransition = transition as TMAutomataTransition
+        return {
+          ...baseTransition,
+          write: tmTransition.write ? { _text: tmTransition.write } : {},
+          move: tmTransition.direction ? { _text: tmTransition.direction } : {}
+        }
+      }
+    }
+  }
   // Sanity check for project type
   if (!Object.keys(PROJECT_TYPE_MAP).includes(projectType)) {
     throw new Error(`Unsupported Automatarium project type "${projectType}"!`)
@@ -44,50 +84,15 @@ const mapTransitions = (transitions: AutomataTransition[], projectType: ProjectT
 
   return transitions.flatMap((transition) => {
     if (transition.read === '') {
-      return [{
-        from: { _text: transition.from },
-        to: { _text: transition.to },
-        read: {}
-      }]
+      return addProjectSpecificAttributes(transition, '', projectType)
     }
 
     // Automatarium stores transitions like [a-z] in a single transition, but it is best to
     // map these individually to a corresponding JFLAP transition
     const readSymbols = expandReadSymbols(transition.read).split('')
-    return readSymbols.map((readSymbol) => {
-      const baseTransition: BaseJFLAPTransition = {
-        from: { _text: transition.from },
-        to: { _text: transition.to },
-        read: readSymbol ? { _text: readSymbol } : {}
-      }
-
-      switch (projectType) {
-        // No extra properties needed for FSA
-        case "FSA": {
-          return baseTransition
-        }
-
-        // Add pop and push properties to transition
-        case "PDA": {
-          const pdaTransition = transition as PDAAutomataTransition
-          return {
-            ...baseTransition,
-            push: pdaTransition.push ? { _text: pdaTransition.push } : {},
-            pop: pdaTransition.pop ? { _text: pdaTransition.pop } : {}
-          }
-        }
-
-        // Add write and move properties to transition
-        case "TM": {
-          const tmTransition = transition as TMAutomataTransition
-          return {
-            ...baseTransition,
-            write: tmTransition.write ? { _text: tmTransition.write } : {},
-            move: tmTransition.direction ? { _text: tmTransition.direction } : {}
-          }
-        }
-      }
-    })
+    return readSymbols.map((readSymbol) =>
+      addProjectSpecificAttributes(transition, readSymbol, projectType)
+    )
   })
 }
 
@@ -103,7 +108,7 @@ export const convertAutomatariumToJFLAP = (automatariumProject: Project): string
     _declaration: {
       _attributes: { version: '1.0', encoding: 'UTF-8', standalone: 'no' }
     },
-    _comment: "Created with love from Automatarium for JFLAP",
+    _comment: 'Created with love from Automatarium for JFLAP',
     structure: {
       type: PROJECT_TYPE_MAP[automatariumProject.projectType],
       automaton: {
--- packages/jflap-translator/src/types/JFLAPTypes.ts
+++ packages/jflap-translator/src/types/JFLAPTypes.ts
@@ -1,4 +1,3 @@
-
 export interface JFLAPState {
   _attributes: {
     id: number
@@ -7,8 +6,8 @@ export interface JFLAPState {
   label?: { _text: string }
   x: { _text: number }
   y: { _text: number }
-  final?: {}
-  initial?: {}
+  final?: object
+  initial?: object
 }
 
 export interface BaseJFLAPTransition {
@@ -24,14 +23,13 @@ export interface JFLAPTransitionPDA extends BaseJFLAPTransition {
   pop: { _text?: string }
 }
 
-export type JFLAPTransition = JFLAPTransitionFSA | JFLAPTransitionPDA | JFLAPTransitionTM
-
 export type JFLAPDirection = 'L' | 'R' | 'S'
-
 export interface JFLAPTransitionTM extends BaseJFLAPTransition {
   write: { _text?: string }
   move: { _text?: JFLAPDirection }
 }
+
+export type JFLAPTransition = JFLAPTransitionFSA | JFLAPTransitionPDA | JFLAPTransitionTM
 
 export interface JFLAPComment {
   text: { _text: string }
