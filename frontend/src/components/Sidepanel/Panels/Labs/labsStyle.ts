451e6dade10231ea6e96a2d9fc53bf0af9d3c130
Liam Keenan <s3943923@student.rmit.edu.au>
2024-09-22 12:01:19 +1000

    Implemented Add Question Modal

--- frontend/src/components/Sidepanel/Panels/Labs/labsStyle.ts
+++ frontend/src/components/Sidepanel/Panels/Labs/labsStyle.ts
@@ -198,15 +198,6 @@ export const WarningLabel = styled('div')`
   color: white;
 `;
 
-// Switch label styling for lab settings
-export const Preference = styled('label')`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0.6rem 0;
-  font-size: 1rem;
-  cursor: pointer;
-`;
 
 export const ButtonContainer = styled('div')`
   display: flex;
@@ -230,46 +221,26 @@ export const ButtonContainer = styled('div')`
   }
 `;
 
-export const EditQuestionContainer = styled('div')`
+
+// Generic field wrapper, for add question modal. 
+export const FieldWrapper = styled('div')`
   display: flex;
-  flex-direction: column;
-  gap: 1rem; /* Adds spacing between elements */
-  margin-top: 1.5rem;
-`;
+  align-items: center;
+  justify-content: space-between; 
+  margin: 0.75rem 0;
+  padding: .2rem 1rem;
 
+  span {
+    color: var(--primary-text);
+    font-size: 1rem;
+    white-space: nowrap;  
+  }
 
-export const Select = styled('select')`
-  width: 100%;
-  padding: 0.6rem;
-  font-size: 1rem;
-  border: 1px solid var(--border);
-  border-radius: 0.3rem;
-  background-color: var(--toolbar); /* Consistent dark background */
-  color: white; /* White text */
-`;
-
-export const OptionInput = styled('input')`
-  width: 100%;
-  padding: 0.6rem 0;
-  font-size: 1rem;
-  border: 1px solid var(--border);
-  border-radius: 0.3rem;
-  margin-bottom: 0.5rem;
-  background-color: var(--toolbar); /* Same background */
-  color: white; /* Consistent text color */
-`;
-
-export const OptionButton = styled('button')`
-  margin-top: 0.5rem;
-  width: 100%;
-  padding: 0.6rem;
-  font-size: 1rem;
-  border-radius: 0.3rem;
-  background-color: var(--primary);
-  color: white;
-  cursor: pointer;
-
-  &:hover {
-    background-color: var(--primary-dark);
+  select {
+    flex-shrink: 0;
+    width: 100px;  
+    padding: 0.4rem;
+    border: 1px solid var(--border);
+    border-radius: 0.25rem;
   }
 `;
