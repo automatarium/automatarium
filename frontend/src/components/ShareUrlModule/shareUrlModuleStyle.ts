1e3e54f26879cfd202e28a5f8daf4c186399a3f9
Dib <79500311+tdib@users.noreply.github.com>
2025-04-10 20:59:04 +1000

    Update styling for share modals

--- frontend/src/components/ShareUrlModule/shareUrlModuleStyle.ts
+++ frontend/src/components/ShareUrlModule/shareUrlModuleStyle.ts
@@ -1,14 +1,14 @@
 import { styled } from 'goober'
 
 export const CopySuccessDiv = styled('div')`
-  color: green;
+  color: var(--success);
 `
 
 export const CopyRowWrapper = styled('div')`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-contents: space-between;
+  justify-content: space-between;
   gap: 0.5em;
   padding-bottom: 0.5em;
 `
