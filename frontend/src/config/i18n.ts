4fa4fdf5c7423833094ff1be8e51875e92253809
s3840285 <s3840285@student.rmit.edu.au>
2025-04-24 15:35:22 +1000

    Initial i18next config file

---
+++ frontend/src/config/i18n.ts
@@ -0,0 +1,16 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+i18n.use(initReactI18next).init({
+  fallbackLng: 'en',
+  resources: {
+    en: {
+
+    }
+  },
+  interpolation: {
+    escapeValue: false
+  }
+})
+
+export default i18n
