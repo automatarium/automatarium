36204d98d050040f7a8a6ab1fe6ace63ad93545e
Aliyah Santiano <143683312+alyuhhh@users.noreply.github.com>
2025-09-25 16:15:50 +1000

    Change to HashRouter for RMIT library

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode, Suspense, createElement, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { setup } from 'goober'
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
+import { HashRouter, Route, Routes, useLocation } from 'react-router-dom'
 
 import * as Pages from './pages'
 
@@ -91,9 +91,9 @@ const basename = process.env.SUB_PATH ?? "/"
 ReactDOM.render(
   <StrictMode>
     <Suspense fallback={<div>Loading</div>}>
-      <BrowserRouter {...(basename ? { basename } : {})}>
+      <HashRouter {...(basename ? { basename } : {})}>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Suspense>
   </StrictMode>,
   document.getElementById('app')
