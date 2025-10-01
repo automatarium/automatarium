e9014ad03f0a872d4684791006095269e5964259
Daniel Wang <103014684+cloudyapple4@users.noreply.github.com>
2025-10-01 16:44:21 +1000

    Remove glob search from build-sw

--- frontend/build-sw.js
+++ frontend/build-sw.js
@@ -1,21 +1,14 @@
 import { injectManifest } from 'workbox-build';
-import { glob } from 'glob';
-
-const distFiles = glob.sync('dist/service-worker*.js');
-if (distFiles.length === 0) {
-  throw new Error('No service worker file found in dist/');
-}
-const sw = distFiles[0];
 
 injectManifest({
-  swSrc: sw,
-  swDest: sw,
+  swSrc: 'dist/service-worker.js',
+  swDest: 'dist/service-worker.js',
   globDirectory: 'dist',
   globPatterns: ["**/*.{js,css,html,svg,png,json,ttf}"],
   maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, 
 }).then(({ count, size, warnings }) => {
   warnings.forEach(console.warn);
-  console.log(`Generated ${sw}, precaching ${count} files, totaling ${size} bytes.`);
+  console.log(`Injected ${count} files, totaling ${size} bytes. (Not counting external files))`);
 }).catch(err => {
   console.error('Error generating service worker:', err);
 });
