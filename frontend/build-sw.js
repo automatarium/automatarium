df86b193c19fe5a1eed823a1d42fc9e66ea4eaa7
Daniel Wang <103014684+cloudyapple4@users.noreply.github.com>
2025-10-08 14:32:48 +1100

    clean up build-sw.js

--- frontend/build-sw.js
+++ frontend/build-sw.js
@@ -2,23 +2,27 @@ import { injectManifest } from 'workbox-build';
 import tourGifs from './src/pages/Tutorials/guidedTour/data/tour-gifs.json' with { type: 'json' };
 const { fsaTourGifs, pdaTourGifs, tmTourGifs } = tourGifs;
 
-
-const formatCacheEntries = (links) => {
+const formatManifestEntries = (links) => {
   return links.map(url => ({url, revision: null}));
 }
 
-const extraCacheEntries = formatCacheEntries([...FSAgifs, ...PDAgifs, ...TMgifs]);
+const additionalManifestEntries = formatManifestEntries([
+  ...fsaTourGifs,
+  ...pdaTourGifs,
+   ...tmTourGifs
+]);
 
+// replaces self.__WB_MANIFEST placeholder with populated manifest
 injectManifest({
   swSrc: 'dist/service-worker.js',
   swDest: 'dist/service-worker.js',
   globDirectory: 'dist',
   globPatterns: ["**/*.{js,css,html,svg,png,json,ttf,webmanifest}"],
-  additionalManifestEntries: extraCacheEntries,
-  maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, 
-}).then(({ count, size, warnings }) => {
+  additionalManifestEntries,
+  maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MiB 
+}).then(({ count, warnings }) => {
   warnings.forEach(console.warn);
-  console.log(`Injected ${count} files, totaling ${size} bytes. (Not counting external files)`);
+  console.log(`Injected ${count} files.`);
 }).catch(err => {
   console.error('Error generating service worker:', err);
 });
