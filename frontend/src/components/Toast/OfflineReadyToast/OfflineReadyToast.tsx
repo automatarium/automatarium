ad7b898cac952d352f7b42e66e7e2c318f80e9ba
Daniel Wang <103014684+cloudyapple4@users.noreply.github.com>
2025-10-14 14:45:01 +1100

    remove unused var

--- frontend/src/components/Toast/OfflineReadyToast/OfflineReadyToast.tsx
+++ frontend/src/components/Toast/OfflineReadyToast/OfflineReadyToast.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { StyledOfflineReadyToast } from "./offlineReadyToastStyle";
 
 export function OfflineReadyToast() {
